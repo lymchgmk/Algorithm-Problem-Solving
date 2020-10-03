ce904226efca01501ec04cb93020539732611614
lymchgmk <lymchgmk@gmail.com>
2020-10-03 17:58:51 +0900

    뚜루루뚜루

---
+++ BJ/분류 안 된 문제들/1260_DFS와 BFS.py
@@ -0,0 +1,47 @@
+import sys
+sys.stdin = open('1260_DFS와 BFS.txt', 'r')
+from collections import deque
+
+
+def bfs(start):
+    global bfs_visited, bfs_result
+    deq = deque()
+    deq.append(start)
+    bfs_visited.append(start)
+
+    while deq:
+        now = deq.popleft()
+        bfs_result.append(now)
+        for next in adj_matrix[now]:
+            if next not in bfs_visited:
+                bfs_visited.append(next)
+                deq.append(next)
+                
+
+def dfs(node):
+    global dfs_visited
+    dfs_visited.append(node)
+
+    for next in adj_matrix[node]:
+        if next not in dfs_visited:
+            dfs(next)
+
+
+N, M, V = map(int, input().split()) #정점의 개수, 간선의 개수, 시작 번호
+
+adj_matrix = [list() for _ in range(N+1)]
+for _ in range(M):
+    x, y = map(int, input().split())
+    adj_matrix[x].append(y)
+    adj_matrix[y].append(x)
+for x in adj_matrix:
+    x.sort()
+
+dfs_visited = []
+bfs_visited, bfs_result = [], []
+
+dfs(V)
+bfs(V)
+
+print(*dfs_visited)
+print(*bfs_result)
--- BJ/삼성 SW 역량 테스트 기출 문제/19238_스타트 택시.py
+++ BJ/삼성 SW 역량 테스트 기출 문제/19238_스타트 택시.py
@@ -1,112 +1,103 @@
 import sys
 sys.stdin = open("19238_스타트 택시.txt", "r")
 
-
 from collections import deque
-from copy import deepcopy
 
-
-# 초기 데이터
-N, M, fuel = map(int, input().split())
-map_data = [list(map(int, input().split())) for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if map_data[i][j] == 1: map_data[i][j] = -1
-taxi = list(map(lambda x: x-1, map(int, input().split())))
-customers_data = sorted([list(map(lambda x: x-1, map(int, input().split()))) for _ in range(M)], key = lambda x : (x[0], x[1]))
-
-
-# 함수
-def is_safe(start, test_map_data):
-    L, x, y = len(map_data), start[0], start[1]
-    return 0 <= x < L and 0 <= y < L and test_map_data[x][y] != -1
-
-
-# 거리 측정하는 함수
-def measure_distance(start, end, map_data):
-    distance_map = deepcopy(map_data)
-
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+def take_taxi(input_taxi):
+    global map_data, taxi
+    map_data[taxi[0]][taxi[1]] = 0
 
     deq = deque()
-    deq.append(start)
+    deq.append(input_taxi)
 
+    visited = []
+    dirs = ((0, -1), (-1, 0), (0, 1), (1, 0))
     while deq:
-        temp = deq.popleft()
-        x, y = temp[0], temp[1]
-        
-        if x == end[0] and y == end[1]: 
-            return distance_map[x][y]
+        temp_x, temp_y = deq.popleft()
+        if type(map_data[temp_x][temp_y]) == list:
+            taxi = [temp_x, temp_y]
+            result = [0, map_data[temp_x][temp_y]]
+            map_data[temp_x][temp_y] = 0
+            return result
 
-        for i in range(4):
-            test_x, test_y = x + dx[i], y + dy[i]
-            test = [test_x, test_y]
+        for d in dirs:
+            test_x, test_y = temp_x + d[0], temp_y + d[1]
+            if 0<=test_x<N and 0<=test_y<N and map_data[test_x][test_y] != -1:
+                if type(map_data[test_x][test_y]) == list:
+                    taxi = [test_x, test_y]
+                    result = [map_data[temp_x][temp_y] + 1, map_data[test_x][test_y]]
+                    map_data[test_x][test_y] = 0
+                    return result
+                else:
+                    test = [test_x, test_y]
+                    if test not in visited:
+                        visited.append(test)
+                        map_data[test_x][test_y]=map_data[temp_x][temp_y] + 1
+                        deq.append(test)
+    return -1
 
-            if 0 <= test_x < N and 0 <= test_y < N:
-                if distance_map[test_x][test_y] != -1 and distance_map[test_x][test_y] == 0:
-                    distance_map[test_x][test_y] = distance_map[x][y] + 1
-                    if distance_map[test_x][test_y] > fuel:
-                        return -1
-                    deq.append(test)
+
+def ride_taxi(input_taxi, input_destination):
+    global map_data, taxi
+    map_data[input_taxi[0]][input_taxi[1]] = 0
+
+    if input_taxi == input_destination:
+        taxi = input_destination
+        return 0
+
+    deq = deque()
+    deq.append(input_taxi)
+
+    visited = []
+    recover = []
+    dirs = ((0, -1), (-1, 0), (0, 1), (1, 0))
+    while deq:
+        temp_x, temp_y = deq.popleft()
+        for d in dirs:
+            test_x, test_y = temp_x + d[0], temp_y + d[1]
+            if 0<=test_x<N and 0<=test_y<N and map_data[test_x][test_y] != -1:
+                if [test_x, test_y] == input_destination:
+                    for r in recover:
+                        map_data[r[0]][r[1]] = r[2:]
+                    taxi = input_destination
+                    return map_data[temp_x][temp_y] + 1
+                else:
+                    if [test_x, test_y] not in visited:
+                        visited.append([test_x, test_y])
+                        if type(map_data[test_x][test_y]) == list:
+                            recover.append([test_x, test_y] + map_data[test_x][test_y])
+                        map_data[test_x][test_y] = map_data[temp_x][temp_y] + 1
+                        deq.append([test_x, test_y])
+    return -1
 
 
-# 내 데이터
-is_arrived = [False] * M
-customers_distance = [-1] * M
-flag = False
-count = 0
+N, M, fuel = map(int, input().split())
+map_data = [list(-x for x in map(int, input().split())) for _ in range(N)]
+taxi = list(map(lambda x: x-1 ,map(int, input().split())))
+passenger_data = [list(map(lambda x: x-1 ,map(int, input().split()))) for _ in range(M)]
+for p_data in passenger_data:
+    map_data[p_data[0]][p_data[1]] = p_data[2:]
 
-while count < M:
-    # 최소거리 손님 탐색
-    for i in range(M):
-        if not is_arrived[i]:
-            customer = customers_data[i]
-            where_is_customer = customer[:2]
-            d = measure_distance(taxi, where_is_customer, map_data)
-            if d == -1: 
-                flag = True
-                break
-
-            customers_distance[i] = measure_distance(taxi, where_is_customer, map_data)
-        else:
-            customers_distance[i] = float('INF')
-
-    if flag == True:
+flag, is_arrived = False, 0
+while is_arrived != M:
+    passenger = take_taxi(taxi)
+    if passenger == -1 or fuel < passenger[0]:
+        flag = True
         break
-
-    for i, d in enumerate(customers_distance):
-        min_d = min(customers_distance)
-        if min_d != -1:
-            # 손님 태움
-            if d == min_d and d < fuel:
-                who_takes_taxi, arrival_location, taxi = i, customers_data[i][2:], customers_data[i][:2]
-                fuel -= d
-                break
-        else:
+    else:
+        distance, destination = passenger[0], passenger[1]
+        fuel -= distance
+    
+        lets_ride = ride_taxi(taxi, destination)
+        if lets_ride == -1 or fuel < lets_ride:
             flag = True
             break
-    if flag == True:
-        break
+        else:
+            distance = lets_ride
+            fuel += distance
+            is_arrived += 1
 
-    # 도착지까지 갈 수 있나?
-    destination_distance = measure_distance(taxi, arrival_location, map_data)
-    if destination_distance == -1:
-        flag = True
-        break
-
-    if destination_distance <= fuel:
-        fuel -= destination_distance
-        is_arrived[who_takes_taxi] = True
-        taxi = arrival_location
-        fuel += 2 * destination_distance
-    else:
-        flag = True
-        break
-
-    count += 1
-
-if flag is True: result = -1
-else: result = fuel
-
-print(result)
+if flag == True:
+    print(-1)
+else:
+    print(fuel)
