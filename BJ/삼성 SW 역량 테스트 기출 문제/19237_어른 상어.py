09fb5975252ad9a2cb0267a9dbdc07277eced7e4
lymchgmk <lymchgmk@gmail.com>
2020-10-09 17:51:00 +0900

    update

--- BJ/삼성 SW 역량 테스트 기출 문제/19237_어른 상어.py
+++ BJ/삼성 SW 역량 테스트 기출 문제/19237_어른 상어.py
@@ -2,16 +2,19 @@ import sys
 sys.stdin = open('19237_어른 상어.txt', 'r')
 
 N, M, k = map(int, input().split())
-jaws_data = [list(map(int, input().split())) for _ in range(N)]
+jaws_array = [list(map(int, input().split())) for _ in range(N)]
 jaws_dirs = list(map(int, input().split()))
-jaws_dir_priority = [list(map(int, input().split())) for _ in range(4*M)]
+dirs_priority = []
+for i in range(M):
+    dirs_priority.append([list(map(int, input().split())) for _ in range(4)])
+
+print(dirs_priority)
 
 for z in range(N):
-    print(jaws_data[z])
+    print(jaws_array[z])
 print()
 
-while True:
-    continue
+jaws_in_the_array= [True] * M
 
 def select_dir():
     pass
@@ -21,3 +24,4 @@ def get_out_jaws():
 
 def odor():
     pass
+
