60ba6022e6cd9cf4e51e23123725626e9f20041c
lymchgmk <lymchgmk@gmail.com>
2020-11-04 21:12:24 +0900

    DP 푸는 중

--- BJ/단계별로 풀어보기/동적 계획법 1/1149_RGB거리.py
+++ BJ/단계별로 풀어보기/동적 계획법 1/1149_RGB거리.py
@@ -1,13 +1,13 @@
 import sys
-sys.stdin = open('1904_01타일.txt', 'rt')
+sys.stdin = open('1149_RGB거리.txt', 'rt')
 
 
 N = int(input())
+houses = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+DP = [houses[0]] + [[0, 0, 0] for _ in range(N-1)]
+for i in range(1, N):
+    DP[i][0] = min(DP[i-1][1], DP[i-1][2]) + houses[i][0]
+    DP[i][1] = min(DP[i-1][0], DP[i-1][2]) + houses[i][1]
+    DP[i][2] = min(DP[i-1][0], DP[i-1][1]) + houses[i][2]
 
-
-def zero_one(n):
-    if n <= 2:
-        return dp[n]
-    else:
-        
-dp = [0, 1, 2]
+print(min(DP[N-1]))
