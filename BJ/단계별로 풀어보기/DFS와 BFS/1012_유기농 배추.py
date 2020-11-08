59672cdc329128c8c56f2ecd0d581e8dda4323a4
lymchgmk <lymchgmk@gmail.com>
2020-11-09 08:19:20 +0900

    BFS에서 count는 별로 좋은 방법이 아니다

--- BJ/단계별로 풀어보기/DFS와 BFS/1012_유기농 배추.py
+++ BJ/단계별로 풀어보기/DFS와 BFS/1012_유기농 배추.py
@@ -2,26 +2,35 @@ import sys
 sys.stdin = open('1012_유기농 배추.txt', 'rt')
 
 
-def DFS(start):
-    global visited
-    visited.append(start)
-    x, y = start
-    dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
-    for d in dirs:
-        test_x, test_y = x+d[0], y+d[1]
-        if 0<=test_x<M and 0<=test_y<N and field[test_x][test_y] == 1:
-            test = [test_x, test_y]
-            if test not in visited:
-                DFS(test)
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
 
+
+def DFS(x, y):
+    global field
+    dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    field[x][y] = -1
+    for d in dirs:
+        test_x, test_y = x + d[0], y + d[1]
+        if 0<=test_x<N and 0<=test_y<M and field[test_x][test_y] == 1:
+            field[test_x][test_y] = 0
+            DFS(test_x, test_y)
 
 
 T = int(input())
 for _ in range(T):
-    M, N, K = map(int, input().split())
-    field = [[0 for _ in range(M)] for _ in range(N)]
+    M, N, K = map(int, input().strip().split())
+    field = [[0]*M for _ in range(N)]
+    cnt = 0
     for _ in range(K):
-        X, Y = map(int, input().split())
-        field[X][Y] = 1
-    visited = []
+        X, Y = map(int, input().strip().split())
+        field[Y][X] = 1
+    
+    for i in range(N):
+        for j in range(M):
+            if field[i][j] == 1:
+                DFS(i, j)
+                cnt += 1
+    
+    print(cnt)
     
