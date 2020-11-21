535a4e981112da84b51e2f9f26da358b18c40122
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-11-21 19:04:50 +0900

    코드 길이 줄임

--- BJ/단계별로 풀어보기/분할 정복/10830_행렬 제곱.py
+++ BJ/단계별로 풀어보기/분할 정복/10830_행렬 제곱.py
@@ -7,18 +7,6 @@ N, B = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 
 
-def square_array(A):
-    res = [[0]*N for _ in range(N)]
-    for i in range(N):
-        row = A[i]
-        for j in range(N):
-            col = [row[j] for row in A]
-            for k in range(N):
-                res[i][j] += row[k]*col[k]
-                res[i][j] %= 1000
-    return res
-
-
 def mul_array(X, Y):
     res = [[0]*N for _ in range(N)]
     for i in range(N):
@@ -34,7 +22,8 @@ def mul_array(X, Y):
 bin_B = bin(B)[2:]
 squared_A = [A]
 for _ in range(len(bin_B)-1):
-    squared_A.append(square_array(squared_A[-1]))
+    squared_last_A = squared_A[-1]
+    squared_A.append(mul_array(squared_last_A, squared_last_A))
 
 answer = [[0]*N for _ in range(N)]
 for i in range(N):
