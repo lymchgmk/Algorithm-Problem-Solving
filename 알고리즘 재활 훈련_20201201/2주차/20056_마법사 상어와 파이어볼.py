dc095bf06449b1e51e0295f05237b646323b3b7c
lymchgmk <lymchgmk@gmail.com>
2020-12-07 12:05:48 +0900

    시간초과

--- 알고리즘 재활 훈련_20201201/2주차/20056_마법사 상어와 파이어볼.py
+++ 알고리즘 재활 훈련_20201201/2주차/20056_마법사 상어와 파이어볼.py
@@ -2,3 +2,12 @@ import sys
 sys.stdin = open('20056_마법사 상어와 파이어볼.txt', 'rt')
 input = lambda: sys.stdin.readline().strip()
 from collections import deque
+
+
+def subset(lst):
+    L = len(lst)
+    result = []
+    for i in range(1<<L):
+        for j in range(L):
+            temp = [x for x in lst if i & (1 << j)]
+            print(temp)
