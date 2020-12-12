04f946465c7fb90078cb37c89f50bd739e2a8e9f
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-12-12 17:30:32 +0900

    동적계획법2 푸는 중

--- BJ/단계별로 풀어보기/동적 계획법 2/7579_앱.py
+++ BJ/단계별로 풀어보기/동적 계획법 2/7579_앱.py
@@ -4,8 +4,17 @@ input = lambda: sys.stdin.readline().strip()
 
 
 N, M = map(int, input().split())
-M = list(map(int, input().split()))
+A = list(map(int, input().split()))
+A.sort()
 C = list(map(int, input().split()))
 
-print(M)
-print(C)
+dp_ks = [[0]*(N+1) for _ in range(N+1)]
+for i in range(N):
+    for j in range(N):
+        w, v = C[i], A[i]
+        if j < w:
+            dp_ks[i][j] = dp_ks[i-1][j]
+        else:
+            dp_ks[i][j] = max(dp_ks[i-1][j-w] + v, dp_ks[i-1][j])
+
+print(dp_ks)
