4dad99a46b87adb5a5923c19690596454f6e25de
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-12-15 21:22:22 +0900

    알고리즘 재활훈련 3주차 3/3 푸는 중

--- 알고리즘 재활 훈련_20201201/3주차/20061_모노미노도미노 2.py
+++ 알고리즘 재활 훈련_20201201/3주차/20061_모노미노도미노 2.py
@@ -3,6 +3,38 @@ sys.stdin = open('20061_모노미노도미노 2.txt', 'rt')
 input = lambda: sys.stdin.readline().strip()
 
 
+def get_point():
+    def blue_point(blue_board):
+        L = len(blue_board)
+        point = 0
+        while True:
+            for i in range(L-1, 1, -1):
+                col = [row[i] for row in blue_board]
+                if sum(col) == 4:
+                    point += 1
+                    break
+            for i in range(L-1, 1, -1):
+                
+
+    pass
+        
+
+
+def cut_board():
+    pass
+
+
+
 N = int(input())
+
+blue_board = [[0]*6 for _ in range(4)]
+green_board = [[0]*4 for _ in range(6)]
+
+row = [row for row in blue_board]
+col = [row[i] for i in range(len())]
+
+point = 0
 for _ in range(N):
+    red_board = [[0]*4 for _ in range(4)]
     t, x, y = map(int, input())
+
