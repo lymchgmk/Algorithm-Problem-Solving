c6545e3b3bd8cd3d57441abbccc7af7805234be3
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-12-19 18:20:58 +0900

    REFERENCE | bellman-ford

--- reference/shortest_path_problem.py
+++ reference/shortest_path_problem.py
@@ -5,6 +5,32 @@
 # 데이크스트라 알고리즘 : 단일-쌍, 단일-출발, 단일-도착
 
 # 벨먼-포드 알고리즘 : 가중치가 음수라면 단일-출발
+def bellman_ford():
+    global isUpdated
+
+    for repeat in range(N):
+        for i in range(1, N+1):
+            for b, c in adj[i]:
+                if dist[i] != INF and dist[b] > dist[i] + c:
+                    dist[b] = dist[i] + c
+                    if repeat == N-1:
+                        isUpdated = False
+    
+    return
+
+
+N, M = map(int, input().split())
+INF = float('inf')
+dist = [INF]*(N+1)
+dist[1] = 0
+isUpdated = True
+
+adj = {i:[] for i in range(N+1)}
+for _ in range(M):
+    A, B, C = map(int, input().split())
+    adj[A].append([B, C])
+
+bellman_ford()
 
 # A* 탐색 알고리즘 : 탐색 속도 향상을 위한 휴리스틱 방법, 단일-쌍
 
