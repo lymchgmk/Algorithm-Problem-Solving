0bee9f56b4a7e04014d37867af4141bcffdf1c05
lymchgmk <lymchgmk@gmail.com>
2021-01-10 20:52:33 +0900

    스택으로 큐 구현

---
+++ 파이썬 알고리즘 인터뷰/9장_스택, 큐/23_큐를 이용한 스택 구현.py
@@ -0,0 +1,28 @@
+from typing import Collection
+
+
+import collections
+
+
+class MyStack:
+    def __inif__(self):
+        self.q = collections.deque()
+
+    
+    def push(self, x):
+        self.q.append(x)
+        # 요소 삽입 후 맨 앞에 두는 상태로 재정렬
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft)
+    
+
+    def pop(self):
+        return self.q.popleft()
+
+    
+    def top(self):
+        return self.q[0]
+    
+
+    def empty(self):
+        return len(self.q) == 0
