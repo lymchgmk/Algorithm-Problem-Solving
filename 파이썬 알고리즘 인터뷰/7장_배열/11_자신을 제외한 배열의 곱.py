8ff4c67a782bd41dbcde42254b83b1d75497ee56
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-21 10:28:37 +0900

    Create 11_자신을 제외한 배열의 곱.py

---
+++ 파이썬 알고리즘 인터뷰/7장_배열/11_자신을 제외한 배열의 곱.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+# 풀이 1. 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    out = []
+    p = 1
+    # 왼쪽 곱셈
+    for i in range(0, len(nums)):
+        out.append(p)
+        p *= nums[i]
+    p = 1
+    # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+    for i in range(len(nums)-1, -1, -1):
+        out[i] *= p
+        p *= nums[i]
+    
+    return out
