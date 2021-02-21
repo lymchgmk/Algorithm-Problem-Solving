7f09b445dd7a015f7352b0cd476fe466c8ba2379
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-21 18:42:09 +0900

    Create 19_역순 연결 리스트 2.py

---
+++ 파이썬 알고리즘 인터뷰/8장_연결 리스트/19_역순 연결 리스트 2.py
@@ -0,0 +1,25 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+        
+# 풀이 1. 반복 구조로 노드 뒤집기
+def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+    # 예외 처리
+    if not head or m == n:
+        return head
+    
+    root = start = ListNode(None)
+    root.next = head
+    # start, end 지정
+    for _ in range(m-1):
+        start = start.next
+    end = start.next
+    
+    # 반복하면서 노드 차례대로 뒤집기
+    for _ in range(n-m):
+        tmp, start.next, end.next = start.next, end.next, end.next.next
+        start.next.next = tmp
+    
+    return root.next
