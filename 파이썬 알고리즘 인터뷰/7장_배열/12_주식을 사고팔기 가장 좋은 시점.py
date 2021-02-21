1ddfc598f3da917a47b7981946ea4100508962b7
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-21 15:16:47 +0900

    Create 12_주식을 사고팔기 가장 좋은 시점.py

---
+++ 파이썬 알고리즘 인터뷰/7장_배열/12_주식을 사고팔기 가장 좋은 시점.py
@@ -0,0 +1,26 @@
+from typing import List
+import sys
+
+
+# 풀이 1. 브루트 포스로 계산
+def maxProfit(self, prices: List[int]) -> int:
+    max_price = 0
+    
+    for i, price in enumerate(prices):
+        for j in range(i, len(prices)):
+            max_price = max(prices[j] - price, max_price)
+    
+    return max_price
+
+
+# 풀이 2. 저점과 현재 값과의 차이 계산
+def maxProfit(self, prices: List[int]) -> int:
+    profit = 0
+    min_price = sys.maxsize
+    
+    # 최솟값과 최댓값을 계속 갱신
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+    
+    return profit
