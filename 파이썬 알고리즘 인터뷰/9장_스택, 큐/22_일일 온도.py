16be3972d106b241a60f47b3bfa1e4b6cc92d652
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-21 19:14:39 +0900

    Create 22_일일 온도.py

---
+++ 파이썬 알고리즘 인터뷰/9장_스택, 큐/22_일일 온도.py
@@ -0,0 +1,15 @@
+# 풀이 1. 스택 값 비교
+from typing import List
+
+
+def dailyTemperatures(self, T: List[int]) -> List[int]:
+    answer = [0]*len(T)
+    stack = []
+    for i, cur in enumerate(T):
+        # 현재 온도가 스택 값보다 높다면 정답 처리
+        while stack and cur > T[stack[-1]]:
+            last = stack.pop()
+            answer[last] = i - last
+        stack.append(i)
+    
+    return answer
