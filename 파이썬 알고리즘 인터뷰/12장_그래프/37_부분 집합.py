085ea17b70a4d44882c763a9eccd0f88e8511434
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-22 20:53:28 +0900

    Create 37_부분 집합.py

---
+++ 파이썬 알고리즘 인터뷰/12장_그래프/37_부분 집합.py
@@ -0,0 +1,19 @@
+from typing import List
+import itertools
+
+
+# 풀이 1. 트리의 모든 DFS 결과
+def subsets(self, nums: List[int]) -> List[List[int]]:
+    result = []
+    
+    def dfs(index, path):
+        # 매번 결과 추가
+        result.append(path)
+        
+        # 경로를 만들면서 DFS
+        for i in range(index, len(nums)):
+            dfs(i+1, path+[nums[i]])
+        
+    dfs(0, [])
+    
+    return result
