e2999a4ff6f4a3df2aad5492af5eded214d2c70a
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-25 20:32:06 +0900

    Create 42_이진 트리의 최대 깊이.py

---
+++ 파이썬 알고리즘 인터뷰/14장_트리/42_이진 트리의 최대 깊이.py
@@ -0,0 +1,32 @@
+import collections
+
+
+class TreeNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.value = val
+        self._leftChild = None
+        self._rightChild = None
+        self.parent = None
+
+
+# 풀이 1. 반복 구조로 BFS 풀이
+def maxDepth(self, root: TreeNode) -> int:
+    if root is None:
+        return 0
+
+    queue = collections.deque([root])
+    depth = 0
+
+    while queue:
+        depth += 1
+        # 큐 연산 추출 노드의 자식 노드 삽입
+        for _ in range(len(queue)):
+            cur_root = queue.popleft()
+            if cur_root.left:
+                queue.append(cur_root.left)
+            if cur_root.right:
+                queue.append(cur_root.right)
+
+    # BFS 반복 횟수 == 깊이
+    return depth
