2b1184182c77462cc01e16f8222e42a307f4f237
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-27 21:22:48 +0900

    Create 51_이진 탐색 트리(BST)를 더 큰 수 합계 트리로.py

---
+++ 파이썬 알고리즘 인터뷰/14장_트리/51_이진 탐색 트리(BST)를 더 큰 수 합계 트리로.py
@@ -0,0 +1,23 @@
+from typing import List
+import collections
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = self.right = 0
+        
+        
+# 풀이 1. 중위 순회로 노드 값 누적
+class Solution:
+    val: int = 0
+    
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 중위 순회 노드 값 누적
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+            
+        return root
