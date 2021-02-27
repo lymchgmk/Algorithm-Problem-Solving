91fba5b862e127bfb50c175ab1ee0fe8918e40a3
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-27 21:30:27 +0900

    Update 53_이진 탐색 트리(BFS) 노드 간 최소 거리.py

--- 파이썬 알고리즘 인터뷰/14장_트리/53_이진 탐색 트리(BFS) 노드 간 최소 거리.py
+++ 파이썬 알고리즘 인터뷰/14장_트리/53_이진 탐색 트리(BFS) 노드 간 최소 거리.py
@@ -1,6 +1,6 @@
 from typing import List
 import collections
-
+import sys
 
 class TreeNode:
     def __init__(self, val):
@@ -8,3 +8,44 @@ class TreeNode:
         self.left = self.right = None
         
         
+# 풀이 1. 재귀 구조로 중위 순회
+class Solution:
+    prev = -sys.maxsize
+    result = sys.maxsize
+    
+    # 재귀 구조 중위 순회 비교 결과
+    def minDiffInBST(self, root: TreeNode) -> int:
+        if root.left:
+            self.minDiffInBST(root.left)
+            
+        self.result = min(self.result, root.val - self.prev)
+        self.prev = root.val
+        
+        if root.right:
+            self.minDiffInBST(root.right)
+            
+        return self.result
+    
+
+# 풀이 2. 반복 구조로 중위 순회
+def minDiffInBST(self, root: TreeNode) -> int:
+    prev = -sys.maxsize
+    result = sys.maxsize
+    
+    stack = []
+    node = root
+    
+    # 반복 구조 중위 순회 비교 결과
+    while stack or node:
+        while node:
+            stack.append(node)
+            node = node.left
+        
+        node = stack.pop()
+        
+        result = min(result, node.val - prev)
+        prev = node.val
+        
+        node = node.right
+        
+    return result
