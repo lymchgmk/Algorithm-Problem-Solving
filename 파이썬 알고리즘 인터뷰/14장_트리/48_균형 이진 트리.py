3bfcd08698a8df35b17bbaabac5a8f3b21799947
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-02-27 19:38:59 +0900

    Create 48_균형 이진 트리.py

---
+++ 파이썬 알고리즘 인터뷰/14장_트리/48_균형 이진 트리.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = self.right = 0
+        
+
+# 풀이 1. 재귀 구조로 높이 차이 계산
+def isBalanced(self, root: TreeNode) -> bool:
+    def check(root):
+        if not root:
+            return 0
+        
+        left = check(root.left)
+        right = check(root.right)
+        # 높이 차이가 나는 경우 -1, 이외에는 높이에 따라 1 증가
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
+    
+    return check(root) != -1
