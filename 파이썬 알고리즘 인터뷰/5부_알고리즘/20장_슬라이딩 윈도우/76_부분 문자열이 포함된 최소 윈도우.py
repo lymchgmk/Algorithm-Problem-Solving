ff6f267477338cee8db7cf5228fde8c87a56b130
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-01 21:51:56 +0900

    Update 76_부분 문자열이 포함된 최소 윈도우.py

--- 파이썬 알고리즘 인터뷰/5부_알고리즘/20장_슬라이딩 윈도우/76_부분 문자열이 포함된 최소 윈도우.py
+++ 파이썬 알고리즘 인터뷰/5부_알고리즘/20장_슬라이딩 윈도우/76_부분 문자열이 포함된 최소 윈도우.py
@@ -1,8 +1,9 @@
 from typing import List
+import collections
 
 
 # 풀이 1. 모든 윈도우 크기를 브루트 포스로 탐색
-def minWindow(self, s: str, t: str):
+def minWindow(self, s: str, t: str) -> str:
     def contains(s_substr_lst: List, t_lst: List):
         for t_elem in t_lst:
             if t_elem in s_substr_lst:
@@ -26,3 +27,26 @@ def minWindow(self, s: str, t: str):
 
 
 # 풀이 2. 투 포인터, 슬라이딩 윈도우로 최적화
+def minWindow(self, s: str, t: str) -> str:
+    need = collections.Counter(t)
+    missing = len(t)
+    left = start = end = 0
+    
+    # 오른쪽 포인터 이동
+    for right, char in enumerate(s, 1):
+        missing -= need[char] > 0
+        need[char] -= 1
+        
+        # 필요 문자가 0이면 왼쪽 포인터 이동 판단
+        if missing == 0:
+            while left < right and need[s[left]] < 0:
+                need[s[left]] += 1
+                left += 1
+            
+            if not end or right - left <= end - start:
+                start, end = left, right
+            need[s[left]] += 1
+            missing += 1
+            left += 1
+            
+    return s[start:end]
