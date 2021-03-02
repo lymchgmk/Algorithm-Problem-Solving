9993bce5f096408798203d3eee7a63a9bf48b39f
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-03 01:48:56 +0900

    Update 81_주유소.py

--- 파이썬 알고리즘 인터뷰/5부_알고리즘/21장_그리디 알고리즘/81_주유소.py
+++ 파이썬 알고리즘 인터뷰/5부_알고리즘/21장_그리디 알고리즘/81_주유소.py
@@ -23,4 +23,17 @@ def conCompleteCircuit_1(self, gas: List[int], cost: List[int]) -> int:
 
 # 풀이 2. 한 번 방문
 def conCompleteCircuit_2(self, gas: List[int], cost: List[int]) -> int:
+    # 모든 주유소 방문 가능 여부 판별
+    if sum(gas) < sum(cost):
+        return -1
     
+    start, fuel = 0, 0
+    for i in range(len(gas)):
+        # 출발점이 안되는 지점 판별
+        if gas[i] + fuel < cost[i]:
+            start = i + 1
+            fuel = 0
+        else:
+            fuel += gas[i] - cost[i]
+    
+    return start
