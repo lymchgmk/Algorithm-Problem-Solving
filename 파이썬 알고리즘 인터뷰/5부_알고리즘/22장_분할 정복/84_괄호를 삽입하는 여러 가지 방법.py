7a95a0a2e72767b48146b6d659501d1b806869ff
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-03 16:32:59 +0900

    Create 84_괄호를 삽입하는 여러 가지 방법.py

---
+++ 파이썬 알고리즘 인터뷰/5부_알고리즘/22장_분할 정복/84_괄호를 삽입하는 여러 가지 방법.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+# 풀이 1. 분할 정복을 이용한 다양한 조합
+def diffWaysToCompute(self, input: str) -> List[int]:
+    def compute(left, right, op):
+        results = []
+        for l in left:
+            for r in right:
+                results.append(eval(str(l) + op + str(r)))
+        return results
+    
+    if input.isdigit():
+        return [int(input)]
+    
+    results = []
+    for index, value in enumerate(input):
+        if value in '-+*':
+            left = self.diffWaysToCompute(input[:index])
+            right = self.diffWaysToCompute(input[index+1:])
+            
+            results.extend(compute(left, right, value))
+    
+    return results
