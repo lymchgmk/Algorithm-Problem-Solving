79e522be07062d0ffe0f1db506e8b2edb1bcc38e
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-10 18:12:16 +0900

    Update bisect.py

--- reference/bisect.py
+++ reference/bisect.py
@@ -20,9 +20,9 @@ def bisect_left(a, x, lo=0, hi=None):
         hi = len(a)
         
     while lo < hi:
-        mid= (lo+hi)//2
+        mid = (lo+hi)//2
         if a[mid] < x:
-            lo= mid+1
+            lo = mid+1
         else:
             hi = mid
     return lo
