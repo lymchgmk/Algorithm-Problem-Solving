1e9f44cc6ce6438adaac89333c23b2a05ded9340
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-19 01:26:18 +0900

    Update 징검다리 건너기.py

--- Progammers/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.py
+++ Progammers/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.py
@@ -1,25 +1,46 @@
-import collections
-
-
-def solution(stones, k):
-    answer = 0
-    _max = max(stones)
-    for window_length in range(_max,  0, -1):
-        window_s, window_e = 0, window_length-1
-        for i in range(window_e, window_s, -1):
-            if stones[i]:
-                window_s = i
-                window_e = i+window_length-1
-                break
-                
-        
-        
+# 풀이 1. 이분탐색
+def solution_1(stones, k):
+    def jump(stones, k, mid):
+        jump_count = k
+        for stone in stones:
+            if stone <= mid:
+                jump_count -= 1
+                if not jump_count:
+                    return False
+            else:
+                jump_count = k
+        return True
     
-        
-            
-    return answer
+    answer = 0
+    left, right = 0, 200000000
+    while left <= right:
+        mid = (left+right)//2
+        if jump(stones, k, mid):
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return answer+1
+
+
+# 풀이 2. 슬라이딩 윈도우
+# 이 경우 max 부분에서 시간이 오버됨?
+def solution_2(stones, k):
+    from collections import deque
+    window = deque(stones[:k])
+    loc_max = max(window)
+    global_min = loc_max
+    for i in range(k, len(stones)):
+        leftpop = window.popleft()
+        window.append(stones[i]) #rightappend
+        if leftpop == loc_max:
+            loc_max = max(window)
+            global_min = min(global_min, loc_max)
+    return global_min
+    
 
 
 stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
 k = 3
-print(solution(stones, k))
+print(solution_1(stones, k))
