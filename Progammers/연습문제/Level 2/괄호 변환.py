2cae9e72370db6b3bda525b324c7a5bb4f26f73d
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-21 20:29:51 +0900

    프로그래머스 / 연습문제 / Level 2

---
+++ Progammers/연습문제/Level 2/괄호 변환.py
@@ -0,0 +1,19 @@
+def solution(p):
+    def proper_brackets:
+        pass
+    
+    def perfect_brackets:
+        pass
+    
+    # 1
+    if not p:
+        return ''
+    # 2
+    u, v = '', ''
+    while p:
+    
+
+
+p = "()))((()"
+print(solution(p))
+print(solution(p) == "()(())()")
