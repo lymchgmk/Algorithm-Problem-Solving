95f778a09236d8b114c18b4452848c25a956bf9a
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-24 06:14:29 +0900

    Create 피보나치 수.py

---
+++ Progammers/연습문제/Level 2/피보나치 수.py
@@ -0,0 +1,29 @@
+def solution(N):
+    SIZE = 2
+    ZERO = [[1, 0], [0, 1]]
+    BASE = [[1, 1], [1, 0]]
+    
+    # 두 행렬의 곱을 구함, O(log2 N)
+    def square_matrix_mul(a, b, size=SIZE):
+        new = [[0 for _ in range(size)] for _ in range(size)]
+        for i in range(size):
+            for j in range(size):
+                for k in range(size):
+                    new[i][j] += (a[i][k] * b[k][j]) % 1234567
+        return new
+    
+    # 기본 행렬을 N번 곱한 행렬을 만든다
+    def get_nth(N):
+        matrix = ZERO.copy()
+        k = 0
+        tmp = BASE.copy()
+        
+        while 2 ** k <= N:
+            if N & (1 << k) != 0:
+                matrix = square_matrix_mul(matrix, tmp)
+            k += 1
+            tmp = square_matrix_mul(tmp, tmp)
+        
+        return matrix
+    
+    return get_nth(N)[1][0] % 1234567
