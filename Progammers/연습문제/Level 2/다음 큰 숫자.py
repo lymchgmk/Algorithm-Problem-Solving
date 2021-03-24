8c22ea058908089e76f9ac9e1b46fa87559c482f
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-24 23:54:13 +0900

    Update 다음 큰 숫자.py

--- Progammers/연습문제/Level 2/다음 큰 숫자.py
+++ Progammers/연습문제/Level 2/다음 큰 숫자.py
@@ -1,7 +1,7 @@
 def solution(n):
-    answer = 0
-    print(bin(n))
-    
+    answer = n+1
+    while str(bin(answer)).count('1') != str(bin(n)).count('1'):
+        answer += 1
     return answer
 
 
