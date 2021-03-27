1b1a24fce8a33443e55a6f22ccddc7522f7b461e
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-28 07:48:08 +0900

    Update [3차] 압축.py

--- Progammers/연습문제/Level 2/[3차] 압축.py
+++ Progammers/연습문제/Level 2/[3차] 압축.py
@@ -1,22 +1,24 @@
 def solution(msg):
-    from collections import deque
     dic = {chr(64+i): i for i in range(1, 27)}
+    buffer = ''
+    result = []
+    for c in msg:
+        test = buffer + c
+        if test in dic:
+            buffer = test
+        else:
+            result.append(dic[buffer])
+            dic[test] = len(dic) + 1
+            buffer = c
     
-    msg = deque(msg)
-    last_val = 26
-    answer = []
-    start = msg.popleft()
-    while msg:
-        while start in dic:
-            start += msg.popleft()
-        last_val += 1
-        dic[start] = last_val
-        answer.append(dic[start[:-1]])
-        start = start[-1]
-    answer.append(dic[start])
-    return answer
+    if buffer:
+        result.append(dic[buffer])
+    
+    return result
+            
+        
     # 1. 없으면 dic에 추가 + 한 칸 줄여서 출력
     # 2. 있으면 계속 늘리기, 없으면 1로
 
-msg = 'TOBEORNOTTOBEORTOBEORNOT'
+msg = 'KAKAO'
 print(solution(msg))
