88f7c72359ff97ad7ebfe11e0516002cb58703c4
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-29 15:19:02 +0900

    Update subset.py

--- reference/subset.py
+++ reference/subset.py
@@ -1,4 +1,8 @@
 def powerset(s):
-	masks = [ 1 << i for i in range(len(s)) ]
+	masks = [1 << i for i in range(len(s))]
 	for i in range( 1 << len(s) ):
-		yield [ss for ss,mask in zip(s,masks) if mask & i ]
+		yield [ss for ss, mask in zip(s, masks) if mask & i]
+		
+		
+		
+print(list(powerset([1,2,3,4])))
