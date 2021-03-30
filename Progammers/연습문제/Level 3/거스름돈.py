0bff808eed10980ee9427dac6c1800d669497ce9
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-03-30 18:30:59 +0900

    Update 거스름돈.py

--- Progammers/연습문제/Level 3/거스름돈.py
+++ Progammers/연습문제/Level 3/거스름돈.py
@@ -1,29 +1,13 @@
 def solution(n, money):
-    answer = 0
-    return answer
+    dp = [1] + [0] * n
+    for coin in money:
+        for price in range(coin, n + 1):
+            if price >= coin:
+                dp[price] += dp[price - coin]
+    
+    return dp[n] % 1000000007
 
 
 n = 5
 money = [1, 2, 5]
 print(solution(n, money))
-
-
-import sys
-sys.stdin = open("12865_평범한 배낭.txt", 'rt')
-
-
-input = lambda: sys.stdin.readline().strip()
-N, K = map(int, input().split())
-stuff = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
-
-
-dp_ks = [[0]*(K+1) for _ in range(N+1)]
-for i in range(1, N+1):
-    for j in range(1, K+1):
-        w, v = stuff[i]
-        if j < w:
-            dp_ks[i][j] = dp_ks[i-1][j]
-        else:
-            dp_ks[i][j] = max(dp_ks[i-1][j-w] + v, dp_ks[i-1][j])
-
-print(dp_ks[N][K])
