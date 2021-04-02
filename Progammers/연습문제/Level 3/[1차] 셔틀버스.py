246811b74abfbae8d2f6208baabd4892fb06cf21
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-02 18:25:49 +0900

    Create [1차] 셔틀버스.py

---
+++ Progammers/연습문제/Level 3/[1차] 셔틀버스.py
@@ -0,0 +1,10 @@
+def solution(n, t, m, timetable):
+    answer = ''
+    return answer
+
+
+n = 1
+t = 1
+m = 5
+timetable = ["08:00", "08:01", "08:02", "08:03"]
+print(solution(n, t, m, timetable))
