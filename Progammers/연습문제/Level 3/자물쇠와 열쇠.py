0847b5e939433ac44ab9ff361f9fcdc3fa99f6c2
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-03 12:12:48 +0900

    Update 자물쇠와 열쇠.py

--- Progammers/연습문제/Level 3/자물쇠와 열쇠.py
+++ Progammers/연습문제/Level 3/자물쇠와 열쇠.py
@@ -3,39 +3,39 @@ def solution(key, lock):
         rotated_keys = [key]
         tmp_key = key
         for angle in range(1, 4):
-            res = [[0] * L for _ in range(L)]
-            for i in range(L):
+            res = [[0] * K for _ in range(K)]
+            for i in range(K):
                 col = [row[i] for row in tmp_key]
-                for j in range(L):
+                for j in range(K):
                     res[i][j] = col[::-1][j]
             rotated_keys.append(res)
             tmp_key = res
         return rotated_keys
     
     def match(lock, rotated_key, point):
-        board = [[0]*(3*L-2) for _ in range(3*L-2)]
-        for i in range(L-1, 2*L-1):
-            for j in range(L-1, 2*L-1):
-                board[i][j] += lock[i-L+1][j-L+1]
+        board = [[0]*(L+2*K-2) for _ in range(L+2*K-2)]
+        for i in range(K-1, K+L-1):
+            for j in range(K-1, K+L-1):
+                board[i][j] += lock[i-K+1][j-K+1]
         
-        for i in range(L):
-            for j in range(L):
+        for i in range(K):
+            for j in range(K):
                 i_rk, j_rk = i+point[0], j+point[1]
                 board[i_rk][j_rk] += rotated_key[i][j]
         
-        for i in range(L-1, 2*L-1):
-            for j in range(L-1, 2*L-1):
+        for i in range(K-1, K+L-1):
+            for j in range(K-1, K+L-1):
                 if board[i][j] != 1:
                     return False
         else:
             return True
         
     
-    L = len(lock)
+    L, K = len(lock), len(key)
     rotated_keys = make_rotated_keys(key)
     for rk in rotated_keys:
-        for i in range(2*L-1):
-            for j in range(2*L-1):
+        for i in range(L+K-1):
+            for j in range(L+K-1):
                 if match(lock, rk, [i, j]):
                     return True
     return False
