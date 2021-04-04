5ab9d03f8aed9690c74d21c4664d95b20240c92c
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-04 21:45:03 +0900

    Create 외벽 점검.py

---
+++ Progammers/연습문제/Level 3/외벽 점검.py
@@ -0,0 +1,13 @@
+def solution(n, weak, dist):
+    answer = 0
+    
+    for i in range(len(weak)):
+        
+    
+    return answer
+
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]
+print(solution(n, weak, dist))
