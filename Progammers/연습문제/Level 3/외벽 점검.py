1619465fa5cce78422b5c90c3c184ead4146342b
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-05 21:31:34 +0900

    Update 외벽 점검.py

--- Progammers/연습문제/Level 3/외벽 점검.py
+++ Progammers/연습문제/Level 3/외벽 점검.py
@@ -3,16 +3,17 @@ from collections import deque
 
 def solution(n, weak, dist):
     def how_many_fix(s, d):
-        res = []
-        for i in range(-d+1, d):
-            tmp = deque(range(1, n+1))
+        res = set([s])
+        for i in range(-d + 1, d):
+            tmp = deque(range(n))
             tmp.rotate(-tmp.index(s) + i)
             if not is_fixed[tmp[0]]:
-                res.append(tmp[0])
+                res.add(tmp[0])
         return res
-        
-    is_fixed = {f: False if f in weak else True for f in range(1, n+1)}
+    
+    is_fixed = {f: False if f in weak else True for f in range(n)}
     answer = 0
+    dist.sort()
     while dist:
         d = dist.pop()
         answer += 1
@@ -20,16 +21,24 @@ def solution(n, weak, dist):
         for nf in need_fix:
             if not nf:
                 need_fix.remove(nf)
-        need_fix.sort(key=lambda x: (-len(x), -(max(x)-min(x))))
+                
+        need_fix.sort(key=lambda x: (-len(x), -(max(x) - min(x))))
+        
         pick = need_fix[0]
+        print(pick)
         for p in pick:
             is_fixed[p] = True
+            
         if all(is_fixed.values()):
             return answer
     return -1
 
 
-n = 12
-weak = [1, 3, 4, 9, 10]
-dist = [3, 5, 7]
+# n = 12
+# weak = [1, 5, 6, 10]
+# dist = [1, 2, 3, 4]
+n = 200
+weak = [0, 10, 50, 80, 120, 160]
+dist = [1, 10, 5, 40, 30]
+3
 print(solution(n, weak, dist))
