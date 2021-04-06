880adae0cc1ebb6474e8542c12875dae44662684
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-06 19:52:21 +0900

    Update 스타 수열.py

--- Progammers/연습문제/Level 3/스타 수열.py
+++ Progammers/연습문제/Level 3/스타 수열.py
@@ -1,33 +1,34 @@
+from collections import Counter
+
+
 def solution(a):
-    def powerset(s):
-        masks = [1 << i for i in range(len(s))]
-        for i in range(1 << len(s)):
-            yield [ss for ss, mask in zip(s, masks) if mask & i]
-    
-    def is_star(x):
-        if len(x) < 2 or len(x) % 2:
-            return False
-        
-        test_inter = [[x[2*i], x[2*i + 1]] for i in range(len(x) // 2)]
-        
-        for t0, t1 in test_inter:
-            if t0 == t1:
-                return False
-            
-        tmp = set(test_inter[0])
-        for i in range(1, len(x)//2):
-            tmp &= set(test_inter[i])
-            if not tmp:
-                return False
-        
-        return True
+    if len(a) <= 1:
+        return 0
     
     answer = 0
-    _powerset = powerset(a)
-    for _ps in _powerset:
-        if is_star(_ps):
-            answer = max(answer, len(_ps))
+    cnt_n = Counter(a).most_common()
+    cnt_n = {n: cnt for n, cnt in cnt_n}
+    
+    for n in cnt_n:
+        if cnt_n[n] * 2 <= answer:
+            continue
+        
+        index = 1
+        cnt = 0
+        while index < len(a):
+            if (a[index - 1] != n and a[index] != n) or a[index - 1] == a[index]:
+                index += 1
+                continue
+            
+            cnt += 2
+            index += 2
+        
+        answer = max(answer, cnt)
+    
     return answer
+
+
+print(solution([1, 2, 3, 1, 4, 5, 6, 1]))
 
 
 a = [0,3,3,0,7,2,0,2,2,0]
