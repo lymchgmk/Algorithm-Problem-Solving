5c2e1f59cf6d1b509a1e4c883196d36ae2897d48
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-08 01:14:34 +0900

    Update [1차] 셔틀버스.py

--- Progammers/연습문제/Level 3/[1차] 셔틀버스.py
+++ Progammers/연습문제/Level 3/[1차] 셔틀버스.py
@@ -3,17 +3,25 @@ from datetime import datetime, time, timedelta
 
 def solution(n, t, m, timetable):
     answer = ''
-    buses = [[datetime(2000, 1, 1, 9, 0, 0) + timedelta(minutes=t)*i, n] for i in range(n)]
-    crews = [time(*map(int, ttb.split(':'))) for ttb in timetable]
-    crews.sort()
+    buses = [[time(hour=9).replace(hour=9+(t*i//60), minute=(t*i) % 60), n] for i in range(n)]
+    timetable.sort()
+    crews = [time.fromisoformat(ttb) for ttb in timetable]
+    print('crews', crews)
+    print('buses', buses)
+    
+    for crew in crews:
+        for bus in buses:
+            if bus[0] >= crew:
+                bus[1] -= 1
+                break
     
     print(buses)
     
     return answer
 
 
-n = 1
-t = 1
-m = 5
-timetable = ["08:00", "08:01", "08:02", "08:03"]
+n = 2
+t = 10
+m = 2
+timetable = ["09:10", "09:09", "08:00"]
 print(solution(n, t, m, timetable))
