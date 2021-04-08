09289de8abf5b01bddc4d857aefa4ab43d428c7c
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-08 14:57:17 +0900

    Update [1차] 셔틀버스.py

--- Progammers/연습문제/Level 3/[1차] 셔틀버스.py
+++ Progammers/연습문제/Level 3/[1차] 셔틀버스.py
@@ -1,28 +1,36 @@
-from datetime import time
+import datetime as dt
 
 
 def solution(n, t, m, timetable):
-    answer = ''
-    buses = [[time(hour=9).replace(hour=9+(t*i//60), minute=(t*i) % 60), n] for i in range(n)]
+    buses = [[dt.time(hour=9).replace(hour=9+(t*i//60), minute=(t*i) % 60), m, []] for i in range(n)]
     timetable.sort()
-    crews = [time.fromisoformat(ttb) for ttb in timetable]
-    print('crews', crews)
-    print('buses', buses)
-    
-    for crew in crews:
+    crews = [dt.time.fromisoformat(tmtb) if tmtb != '24:00' else dt.time(hour=0, minute=0) for tmtb in timetable]
+    for idx, crew in enumerate(crews):
         for bus in buses:
-            if bus[0] >= crew:
+            if bus[1] > 0 and bus[0] >= crew:
                 bus[1] -= 1
+                bus[2].append(idx)
                 break
-    print(buses)
-    
+                
     # 마지막 버스에 자리가 있다 / 없다
+    if buses[-1][1] == 0:
+        last_ride_crew = buses[-1][2][-1]
+        tmp = crews[last_ride_crew].isoformat()
+        
+        tmp_h, tmp_m, _ = map(int, tmp.split(':'))
+        if tmp_m == 0:
+            tmp_h, tmp_m = tmp_h-1, 59
+        else:
+            tmp_m -= 1
+        answer = dt.time(hour=tmp_h, minute=tmp_m).isoformat()[:-3]
+    else:
+        answer = buses[-1][0].isoformat()[:-3]
     
     return answer
-
+    
 
 n = 1
 t = 1
 m = 5
-timetable = ["00:01", "00:01", "00:01", "00:01", "00:01"]
+timetable = ["08:00", "08:01", "08:02", "08:03"]
 print(solution(n, t, m, timetable))
