5508b2c486e941a53261af823e4d2a50b1efc0e2
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-10 19:27:57 +0900

    Update 광고 삽입.py

--- Progammers/연습문제/Level 3/광고 삽입.py
+++ Progammers/연습문제/Level 3/광고 삽입.py
@@ -5,7 +5,6 @@ def solution(play_time, adv_time, logs):
     def adv_run_time(log_start, log_end, adv_start, adv_end):
         if adv_end < log_start or adv_start > log_end:
             return dt.timedelta(seconds=0)
-        
         tmp = sorted([log_start, log_end, adv_start, adv_end])
         return tmp[2] - tmp[1]
     
@@ -14,27 +13,30 @@ def solution(play_time, adv_time, logs):
     adv_h, adv_m, adv_s = map(int, adv_time.split(':'))
     dt_adv_time = dt.timedelta(hours=adv_h, minutes=adv_m, seconds=adv_s)
     dt_logs = []
+    dt_answer_candidates = [[dt.timedelta(seconds=0), dt_adv_time]]
     for log in logs:
         log_start, log_end = log.split('-')
         start_h, start_m, start_s = map(int, log_start.split(':'))
         end_h, end_m, end_s = map(int, log_end.split(':'))
         dt_log_start, dt_log_end = dt.timedelta(hours=start_h, minutes=start_m, seconds=start_s), dt.timedelta(hours=end_h, minutes=end_m, seconds=end_s)
         dt_logs.append([dt_log_start, dt_log_end])
-        
+        if dt_log_end - dt_adv_time >= dt.timedelta(seconds=0):
+            dt_answer_candidates.append([dt_log_end - dt_adv_time, dt_log_end])
+        if dt_log_start + dt_adv_time <= dt_play_time:
+            dt_answer_candidates.append([dt_log_start, dt_log_start + dt_adv_time])
+    
     answer = [dt.timedelta(seconds=0), dt.timedelta(seconds=0)]
-    for adv_start in range((dt_play_time-dt_adv_time).seconds + 1):
-        dt_adv_start = dt.timedelta(seconds=adv_start)
-        dt_adv_end = dt_adv_start + dt_adv_time
-        adv_run_time_sum = dt.timedelta(seconds=0)
-        for dt_log_start, dt_log_end in dt_logs:
-            adv_run_time_sum += adv_run_time(dt_log_start, dt_log_end, dt_adv_start, dt_adv_end)
-        if answer[0] < adv_run_time_sum:
-            answer = [adv_run_time_sum, dt_adv_start]
+    for candidate in dt_answer_candidates:
+        sum_adv_time = dt.timedelta(seconds=0)
+        for log in dt_logs:
+            sum_adv_time += adv_run_time(log[0], log[1], candidate[0], candidate[1])
+        if answer[1] < sum_adv_time or (answer[1] == sum_adv_time and answer[0] > candidate[0]):
+            answer = [candidate[0], sum_adv_time]
 
-    return str(answer[1]).zfill(8)
+    return str(answer[0]).zfill(8)
 
 
-play_time = "02:03:55"
-adv_time = "00:14:15"
-logs = ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]
+play_time = "50:00:00"
+adv_time = "50:00:00"
+logs = ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]
 print(solution(play_time, adv_time, logs))
