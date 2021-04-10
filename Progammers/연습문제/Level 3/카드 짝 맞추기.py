39ba8d90f89d310f4a5fc773ffa489e121791f66
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-10 19:28:00 +0900

    Create 카드 짝 맞추기.py

---
+++ Progammers/연습문제/Level 3/카드 짝 맞추기.py
@@ -0,0 +1,26 @@
+def solution(board, r, c):
+    answer = 0
+    def how_many_control():
+        pass
+    
+    # how_many_control로 눌러야 하는 커서 수 측정
+    
+    # 뒤집은 카드가 없는 경우 / 있는 경우
+    
+    
+    
+    
+    return answer
+
+
+board = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
+r = 1
+c = 0
+print(solution(board, r, c))
+# 14
+
+# board = [[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]]
+# r = 0
+# c = 1
+# print(solution(board, r, c))
+# # 16
