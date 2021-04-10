80227f790db97e57580ef624ecda0ac703dd8cee
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-11 00:01:23 +0900

    Update 카드 짝 맞추기.py

--- Progammers/연습문제/Level 3/카드 짝 맞추기.py
+++ Progammers/연습문제/Level 3/카드 짝 맞추기.py
@@ -1,12 +1,32 @@
-from itertools import permutations
+from itertools import permutations, product
 
 
 def solution(board, r, c):
+    # 카드 한 종류를 (삭제 후의 보드, 커서 위치, 누른 버튼의 총 수)를 리턴해주는 함수를 구현하자 (2개의 답이 나올것)
+    def find_and_flip():
+        return (board, cursor, cnt)
+    # 카드를 다 삭제하면 return
+    
+    # 카드 우선순위 정하기
     cards = set([board[i][j] for i in range(4) for j in range(4)])
     cards.remove(0)
     card_ranks = permutations(cards, len(cards))
+    answer = float('inf')
+    # 우선순위에 따라 카드 삭제
     for card_rank in card_ranks:
-        pass
+        before = [[board, [r, c]]]
+        after = []
+        # 버튼 횟수 측정
+        for target in card_rank:
+            while before:
+                after.append(find_and_flip())
+            before, after = after, []
+        
+        for _, _, cnt in before:
+            answer = min(answer, cnt)
+    
+    return answer
+    
 
 
 board = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
