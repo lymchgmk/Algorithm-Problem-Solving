6bd33650acebabd1f586b7b26209714311843072
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-10 21:39:03 +0900

    Update 카드 짝 맞추기.py

--- Progammers/연습문제/Level 3/카드 짝 맞추기.py
+++ Progammers/연습문제/Level 3/카드 짝 맞추기.py
@@ -1,16 +1,12 @@
+from itertools import permutations
+
+
 def solution(board, r, c):
-    answer = 0
-    def how_many_control():
+    cards = set([board[i][j] for i in range(4) for j in range(4)])
+    cards.remove(0)
+    card_ranks = permutations(cards, len(cards))
+    for card_rank in card_ranks:
         pass
-    
-    # how_many_control로 눌러야 하는 커서 수 측정
-    
-    # 뒤집은 카드가 없는 경우 / 있는 경우
-    
-    
-    
-    
-    return answer
 
 
 board = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
