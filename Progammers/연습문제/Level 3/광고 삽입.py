2e8a2932547827658e400c7874fb86060f3f0f95
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-10 21:19:35 +0900

    Update 광고 삽입.py

--- Progammers/연습문제/Level 3/광고 삽입.py
+++ Progammers/연습문제/Level 3/광고 삽입.py
@@ -2,38 +2,44 @@ import datetime as dt
 
 
 def solution(play_time, adv_time, logs):
-    def adv_run_time(log_start, log_end, adv_start, adv_end):
-        if adv_end < log_start or adv_start > log_end:
-            return dt.timedelta(seconds=0)
-        tmp = sorted([log_start, log_end, adv_start, adv_end])
-        return tmp[2] - tmp[1]
+    def str_to_time(str_time):
+        h, m, s = map(int, str_time.split(':'))
+        return int(dt.timedelta(hours=h, minutes=m, seconds=s).total_seconds())
     
-    play_h, play_m, play_s = map(int, play_time.split(':'))
-    dt_play_time = dt.timedelta(hours=play_h, minutes=play_m, seconds=play_s)
-    adv_h, adv_m, adv_s = map(int, adv_time.split(':'))
-    dt_adv_time = dt.timedelta(hours=adv_h, minutes=adv_m, seconds=adv_s)
-    dt_logs = []
-    dt_answer_candidates = [[dt.timedelta(seconds=0), dt_adv_time]]
+    def time_to_str(int_time):
+        int_h, int_time = divmod(int_time, 3600)
+        int_m, int_s = divmod(int_time, 60)
+        str_h, str_m, str_s = str(int_h).zfill(2), str(int_m).zfill(2), str(int_s).zfill(2)
+        return ':'.join([str_h, str_m, str_s])
+    
+    dt_play_time = str_to_time(play_time)
+    dt_adv_time = str_to_time(adv_time)
+    total_time = [0]*(dt_play_time+1)
+
     for log in logs:
         log_start, log_end = log.split('-')
-        start_h, start_m, start_s = map(int, log_start.split(':'))
-        end_h, end_m, end_s = map(int, log_end.split(':'))
-        dt_log_start, dt_log_end = dt.timedelta(hours=start_h, minutes=start_m, seconds=start_s), dt.timedelta(hours=end_h, minutes=end_m, seconds=end_s)
-        dt_logs.append([dt_log_start, dt_log_end])
-        if dt_log_end - dt_adv_time >= dt.timedelta(seconds=0):
-            dt_answer_candidates.append([dt_log_end - dt_adv_time, dt_log_end])
-        if dt_log_start + dt_adv_time <= dt_play_time:
-            dt_answer_candidates.append([dt_log_start, dt_log_start + dt_adv_time])
-    
-    answer = [dt.timedelta(seconds=0), dt.timedelta(seconds=0)]
-    for candidate in dt_answer_candidates:
-        sum_adv_time = dt.timedelta(seconds=0)
-        for log in dt_logs:
-            sum_adv_time += adv_run_time(log[0], log[1], candidate[0], candidate[1])
-        if answer[1] < sum_adv_time or (answer[1] == sum_adv_time and answer[0] > candidate[0]):
-            answer = [candidate[0], sum_adv_time]
+        dt_log_start, dt_log_end = str_to_time(log_start), str_to_time(log_end)
+        total_time[dt_log_start] += 1
+        total_time[dt_log_end] -= 1
 
-    return str(answer[0]).zfill(8)
+    for i in range(1, dt_play_time):
+        total_time[i] += total_time[i-1]
+
+    for i in range(1, dt_play_time):
+        total_time[i] += total_time[i-1]
+
+    answer, max_time = 0, 0
+    for i in range(dt_adv_time-1, dt_play_time):
+        if i >= dt_adv_time:
+            if max_time < total_time[i] - total_time[i - dt_adv_time]:
+                max_time = total_time[i] - total_time[i - dt_adv_time]
+                answer = i - dt_adv_time + 1
+        else:
+            if max_time < total_time[i]:
+                max_time = total_time[i]
+                answer = i - dt_adv_time + 1
+
+    return time_to_str(answer)
 
 
 play_time = "50:00:00"
