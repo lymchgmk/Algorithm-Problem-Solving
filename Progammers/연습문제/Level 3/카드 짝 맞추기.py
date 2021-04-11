dc4eb989df5339f69ee9a438085e6fa696bb620d
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-11 21:13:50 +0900

    Update 카드 짝 맞추기.py

--- Progammers/연습문제/Level 3/카드 짝 맞추기.py
+++ Progammers/연습문제/Level 3/카드 짝 맞추기.py
@@ -1,10 +1,20 @@
-from itertools import permutations, product
+from itertools import permutations
+from copy import deepcopy
 
 
 def solution(board, r, c):
     # 카드 한 종류를 (삭제 후의 보드, 커서 위치, 누른 버튼의 총 수)를 리턴해주는 함수를 구현하자 (2개의 답이 나올것)
-    def find_and_flip():
-        return (board, cursor, cnt)
+    def find_and_flip(input_board, input_cursor, input_cnt, flip_target):
+        def _find_pos(cur_pos, target_card):
+            return [[i, j] for i in range(4) for j in range(4) if input_board[i][j] == target_card]
+        
+        def _how_many(cur_pos, card_pos):
+            pass
+        
+        def _flip():
+            pass
+        
+        return [[board, [r, c]], cnt]
     # 카드를 다 삭제하면 return
     
     # 카드 우선순위 정하기
@@ -12,22 +22,25 @@ def solution(board, r, c):
     cards.remove(0)
     card_ranks = permutations(cards, len(cards))
     answer = float('inf')
-    # 우선순위에 따라 카드 삭제
+    
+    # 우선순위에 따라
     for card_rank in card_ranks:
-        before = [[board, [r, c]]]
-        after = []
-        # 버튼 횟수 측정
-        for target in card_rank:
-            while before:
-                after.append(find_and_flip())
-            before, after = after, []
+        before_flip = [[board, [r, c]], 0]
+        after_flip = []
         
-        for _, _, cnt in before:
+        #  카드 삭제 및 버튼 횟수 측정
+        for target in card_rank:
+            while before_flip:
+                before_board, before_cursor, before_cnt = before_flip.pop()
+                after_flip.extend(find_and_flip(before_board, before_cursor, before_cnt, target))
+            before_flip, after_flip = after_flip, []
+        
+        # 최소값 갱신
+        for _, _, cnt in before_flip:
             answer = min(answer, cnt)
     
     return answer
     
-
 
 board = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]
 r = 1
