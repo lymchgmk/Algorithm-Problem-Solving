aecf36e48c43175bd5c4462f548a805748480c65
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-13 22:55:17 +0900

    Update [카카오 인턴] 경주로 건설.py

--- Progammers/연습문제/Level 3/[카카오 인턴] 경주로 건설.py
+++ Progammers/연습문제/Level 3/[카카오 인턴] 경주로 건설.py
@@ -3,6 +3,9 @@ from collections import deque
 
 def solution(board):
     def bfs(start_x, start_y, start_dir):
+        bfs_board = [[[board[y][x], None] for x in range(L)] for y in range(L)]
+        bfs_board[0][0][1] = start_dir
+        
         deq = deque([[start_x, start_y, start_dir]])
         while deq:
             cur_x, cur_y, cur_dir = deq.popleft()
@@ -23,17 +26,10 @@ def solution(board):
     
     dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
     L = len(board)
-    # 벽 유무, cost, dir
-    cost_board = [[[board[y][x], 0, None] for x in range(L)] for y in range(L)]
-    cost_board[0][0][2] = 'start_dir'
     
     # start_dir을 아래로 / 오른쪽으로 2가지 경우로 실행한 뒤, 최소값
-    bfs(0, 0, 'start_dir')
-    
-    for c in cost_board:
-        print(list(tmp[1] for tmp in c))
-    
-    return cost_board[L-1][L-1][1]
+    return min(bfs(0, 0, (1, 0)), bfs(0, 0, (0, 1)))
+
 
 
 board = [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]
