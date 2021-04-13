dfed88ddda9cbbe69dfc102d0755b9f960f564f4
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-13 23:06:42 +0900

    Update [카카오 인턴] 경주로 건설.py

--- Progammers/연습문제/Level 3/[카카오 인턴] 경주로 건설.py
+++ Progammers/연습문제/Level 3/[카카오 인턴] 경주로 건설.py
@@ -5,25 +5,25 @@ def solution(board):
     def bfs(start_x, start_y, start_dir):
         bfs_board = [[[board[y][x], None] for x in range(L)] for y in range(L)]
         bfs_board[0][0][1] = start_dir
-        
-        deq = deque([[start_x, start_y, start_dir]])
+        deq = deque([[start_x, start_y]])
         while deq:
-            cur_x, cur_y, cur_dir = deq.popleft()
+            cur_x, cur_y = deq.popleft()
+            cur_dir = bfs_board[cur_x][cur_y][1]
             for nxt_dir in dirs:
                 nxt_x, nxt_y = cur_x + nxt_dir[0], cur_y + nxt_dir[1]
-                if 0 <= nxt_x < L and 0 <= nxt_y < L and not cost_board[nxt_x][nxt_y][0]:
-                    if cur_dir == 'start_dir' or cur_dir == nxt_dir:
-                        nxt_cost = cost_board[cur_x][cur_y][1] + 100
-                    elif cur_dir != nxt_dir:
-                        nxt_cost = cost_board[cur_x][cur_y][1] + 600
+                if 0 <= nxt_x < L and 0 <= nxt_y < L and bfs_board[nxt_x][nxt_y][0] != 1:
+                    if nxt_dir == cur_dir:
+                        nxt_cost = bfs_board[cur_x][cur_y][0] + 100
+                    else:
+                        nxt_cost = bfs_board[cur_x][cur_y][0] + 600
                     
-                    if cost_board[nxt_x][nxt_y][2] != 'start_dir' and not cost_board[nxt_x][nxt_y][1] or cost_board[nxt_x][nxt_y][1] > nxt_cost:
-                        cost_board[nxt_x][nxt_y][1] = nxt_cost
-                        cost_board[nxt_x][nxt_y][2] = nxt_dir
-                        deq.append([nxt_x, nxt_y, nxt_dir])
-                        
+                    if bfs_board[nxt_x][nxt_y][0] == 0 or bfs_board[nxt_x][nxt_y][0] > nxt_cost:
+                        bfs_board[nxt_x][nxt_y][0] = nxt_cost
+                        bfs_board[nxt_x][nxt_y][1] = nxt_dir
+                        deq.append([nxt_x, nxt_y])
+        
+        return bfs_board[-1][-1][0]
 
-    
     dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
     L = len(board)
     
