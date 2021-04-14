4df06482e26b486c5e1e28286ee4a1d9c43feb57
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-15 01:53:52 +0900

    Update 블록 이동하기.py

--- Progammers/연습문제/Level 3/블록 이동하기.py
+++ Progammers/연습문제/Level 3/블록 이동하기.py
@@ -3,30 +3,60 @@ from copy import deepcopy
 
 def solution(board):
     def horizontal_or_vertical(p1, p2):
-        if p1[0] - p2[0]:
-            return 'vertical'
-        else:
-            return 'horizontal'
+        return p1[0] == p2[0]
     
     def bfs(left_wing, right_wing):
         # 현 위치
         # 움직일 수 있는 모든 위치
         # board에 2 입력
         # 위치 교환
+        L = len(board)
         cnt = 0
-        cur_deq = deque([left_wing, right_wing])
+        cur_deq, nxt_deq = deque([left_wing, right_wing]), deque()
         while board[-1][-1] != 2:
-            nxt_deq = deque()
             while cur_deq:
-                cur_left_wing, cur_right_wing = cur_deq.popleft()
+                cur_left_wing, cur_right_wing = sorted(cur_deq.popleft())
+                # horizontal
                 if horizontal_or_vertical(cur_left_wing, cur_right_wing):
-                    pass
+                    left_x, left_y = cur_left_wing
+                    right_x, right_y = cur_right_wing
+                    # 수평
+                    if 0 <= left_y - 1: nxt_deq.append([[left_x - 1, left_y], [right_x - 1, right_y]])
+                    if right_y + 1 < L: nxt_deq.append([[left_x - 1, left_y], [right_x - 1, right_y]])
+                    # 수직
+                    if 0 <= left_x - 1: nxt_deq.append([[left_x - 1, left_y], [right_x - 1, right_y]])
+                    if right_x + 1 < L: nxt_deq.append([[left_x - 1, left_y], [right_x - 1, right_y]])
+                    # 회전
+                    for i in range(2):
+                        for j in range(2):
+                            if 0<=left_x+i<L and 0<=left_y+j<L:
+                                if board[left_x+i][left_y+j] == 1:
+                                    can_rotate = False
+                                    break
+                    else:
+                        can_rotate = True
+                    
+                    if can_rotate:
+                        nxt_deq.append([[left_x, left_y], [left_x + 1, left_y]])
+                        nxt_deq.append([[left_x, left_y + 1], [left_x + 1, left_y + 1]])
+                    
+                        
                 else:
-                    pass
+                    # 수평
+                    # 수직
+                    # 회전
+                
+            for nxt_left_wing, nxt_right_wing in nxt_deq:
+                board[nxt_left_wing[0]][nxt_left_wing[1]] = 2
+                board[nxt_right_wing][0][nxt_right_wing[1]] = 2
+                
+            cur_deq, nxt_deq = nxt_deq, deque()
             
-    
-    bfs([0, 0], [0, 1])
-    return
+            cnt += 1
+
+        return cnt
+
+    return bfs([0, 0], [0, 1])
     
     
 
