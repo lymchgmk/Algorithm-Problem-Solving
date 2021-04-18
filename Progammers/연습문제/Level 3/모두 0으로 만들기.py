1c39e7c850012f63257cd8eb66ebe54f7b7ff5ef
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-18 22:15:14 +0900

    Update 모두 0으로 만들기.py

--- Progammers/연습문제/Level 3/모두 0으로 만들기.py
+++ Progammers/연습문제/Level 3/모두 0으로 만들기.py
@@ -1,39 +1,30 @@
+import sys
+sys.setrecursionlimit(100000)
+
+
 def solution(a, edges):
-    def find_leaf(graph):
-        return [node for node in graph if len(graph[node]) == 1]
-    
-    def find_root(graph):
-        return max(graph, key=lambda x: len(graph[x]))
-    
-    def dfs(a):
-        answer = 0
-        stack = find_leaf(graph)
-        root = find_root(graph)
-        while stack:
-            cur = stack.pop()
-            for nxt in graph[cur]:
-                if nxt == root:
-                    break
-                answer += abs(a[cur])
-                a[nxt] += a[cur]
-                a[cur] = 0
-                stack.append(nxt)
-                
-        return answer
-        
-    if not any(a):
-        return 0
-    
-    if sum(a):
+    if sum(a) != 0:
         return -1
-    
-    graph = {i: [] for i in range(len(a))}
+
+    graph = {idx: [] for idx in range(len(a))}
     for v1, v2 in edges:
         graph[v1].append(v2)
         graph[v2].append(v1)
-    
-    return dfs(a)
-    
+        
+    def dfs(cur, root):
+        nonlocal answer
+        for nxt in graph[cur]:
+            if nxt != root:
+                dfs(nxt, cur)
+
+        a[root] += a[cur]
+        answer += abs(a[cur])
+        a[cur] = 0
+        
+    answer = 0
+    dfs(0, 0)
+
+    return answer
     
 a = [-5,0,2,1,2]
 edges = [[0,1],[3,4],[2,3],[0,3]]
