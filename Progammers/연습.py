8afc458d8f18956d573fd4018c19f178c560e235
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-04-25 19:23:06 +0900

    Update 연습.py

--- Progammers/연습.py
+++ Progammers/연습.py
@@ -1,2 +1,8 @@
-a =[0, 0, 0]
-print(any(a))
+board = [[11,13,15,16],[12,1,4,3],[10,2,7,8],[5,14,6,9]]
+for i, j in zip(range(len(board)), range(len(board) - 1, -1, -1)):
+    print(board[i][j])
+
+print()
+
+for i, j in zip(range(len(board)), range(len(board))):
+    print(board[i][j])
