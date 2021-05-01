4bb5cc6572a8cb7c530bbcd343ea6b7310aa8aa9
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-05-01 23:20:43 +0900

    Update 행렬 테두리 회전하기.py

--- Progammers/연습문제/Level 2/행렬 테두리 회전하기.py
+++ Progammers/연습문제/Level 2/행렬 테두리 회전하기.py
@@ -1,12 +1,22 @@
 def solution(rows, columns, queries):
     def rotate(arr, query):
-        x1, y1, x2, y2 = map(lambda x: x-1, query)
-        print(x1, y1, x2, y2)
+        x1, y1, x2, y2 = map(lambda x: x - 1, query)
+        min_cand, tmp = [], 0
+        for y in range(y1, y2):
+            arr[x1][y], tmp = tmp, arr[x1][y]
+            min_cand.append(tmp)
+        for x in range(x1, x2):
+            arr[x][y2], tmp = tmp, arr[x][y2]
+            min_cand.append(tmp)
+        for y in range(y2, y1, -1):
+            arr[x2][y], tmp = tmp, arr[x2][y]
+            min_cand.append(tmp)
+        for x in range(x2, x1-1, -1):
+            arr[x][y1], tmp = tmp, arr[x][y1]
+            min_cand.append(tmp)
+        return arr, min(min_cand[:-1])
         
-        return arr, 0
-    
-    
-    arr = [[c*rows + r + 1 for r in range(rows)] for c in range(columns)]
+    arr = [[r * columns + c + 1 for c in range(columns)] for r in range(rows)]
     answer = []
     for query in queries:
         arr, min_value = rotate(arr, query)
