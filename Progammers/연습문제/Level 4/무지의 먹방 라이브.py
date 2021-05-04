50ce892caeb49e9f8eb39ebc4daaed882657e6f8
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-05-05 03:23:45 +0900

    Update 무지의 먹방 라이브.py

--- Progammers/연습문제/Level 4/무지의 먹방 라이브.py
+++ Progammers/연습문제/Level 4/무지의 먹방 라이브.py
@@ -1,22 +1,21 @@
 def solution(food_times, k):
     foods = [[food_times[idx], idx+1] for idx in range(len(food_times))]
     foods.sort()
-    
+
     L = len(foods)
-    pretime = 0
+    _min = 0
     for idx, food in enumerate(foods):
-        diff = food[0] - pretime
+        diff = food[0] - _min
         if diff != 0:
-            spend = diff * L
-            if spend <= k:
-                k -= spend
-                pretime = food[0]
+            edible = diff * L
+            if edible <= k:
+                k -= edible
+                _min = food[0]
             else:
-                k %= L
-                sublist = sorted(foods[idx:], key=lambda x: x[1])
-                return sublist[k][1]
+                remain_foods = sorted(foods[idx:], key=lambda x: x[1])
+                return remain_foods[k % L][1]
         L -= 1
-    
+
     return -1
 
 
