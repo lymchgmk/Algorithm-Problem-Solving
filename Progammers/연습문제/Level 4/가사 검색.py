6d1510bf84cca7f11b627fd03fe49a6c99a40ce5
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-05-05 03:57:44 +0900

    Trie

--- Progammers/연습문제/Level 4/가사 검색.py
+++ Progammers/연습문제/Level 4/가사 검색.py
@@ -1,53 +1,46 @@
-# import re
+# from collections import defaultdict
+# from bisect import bisect_left, bisect_right
 #
 #
 # def solution(words, queries):
-#     answer = [0]*len(queries)
-#     for idx, query in enumerate(queries):
-#         pattern = query.replace('?', '.')
-#         for word in words:
-#             if re.fullmatch(pattern, word):
-#                 answer[idx] += 1
+#     def _bisect(lst, start, end):
+#         return bisect_right(lst, end) - bisect_left(lst, start)
+#
+#     answer = []
+#     cands = defaultdict(list)
+#     reversed_cands = defaultdict(list)
+#
+#     for word in words:
+#         cands[len(word)].append(word)
+#         reversed_cands[len(word)].append(word[::-1])
+#
+#     for cand in cands.values():
+#         cand.sort()
+#     for rev_cand in reversed_cands.values():
+#         rev_cand.sort()
+#
+#     for query in queries:
+#         if query[0] == '?':
+#             lst = reversed_cands[len(query)]
+#             start, end = query[::-1].replace('?', 'a'), query[::-1].replace('?', 'z')
+#         else:
+#             lst = cands[len(query)]
+#             start, end = query.replace('?', 'a'), query.replace('?', 'z')
+#
+#         answer.append(_bisect(lst, start, end))
+#
 #     return answer
-#
-#
-# words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
-# queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
-# print(solution(words, queries))
-
-
-from collections import defaultdict
-from bisect import bisect_left, bisect_right
 
 
 def solution(words, queries):
-    def _bisect(lst, start, end):
-        return bisect_right(lst, end) - bisect_left(lst, start)
-        
-    answer = []
-    cands = defaultdict(list)
-    reversed_cands = defaultdict(list)
+    class Node:
+        def __init__(self, key, data):
+            self.key = key
+            self.data = data
+            self.children = {}
     
-    for word in words:
-        cands[len(word)].append(word)
-        reversed_cands[len(word)].append(word[::-1])
-    
-    for cand in cands.values():
-        cand.sort()
-    for rev_cand in reversed_cands.values():
-        rev_cand.sort()
-    
-    for query in queries:
-        if query[0] == '?':
-            lst = reversed_cands[len(query)]
-            start, end = query[::-1].replace('?', 'a'), query[::-1].replace('?', 'z')
-        else:
-            lst = cands[len(query)]
-            start, end = query.replace('?', 'a'), query.replace('?', 'z')
-            
-        answer.append(_bisect(lst, start, end))
-    
-    return answer
+    class Trie:
+        pass
     
     
 words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
