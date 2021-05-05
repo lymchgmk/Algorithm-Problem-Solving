ec2270d0473c883043dbba05a7e5a93aac34a9d4
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-05-05 23:27:53 +0900

    Update 다단계 칫솔 판매.py

--- Progammers/연습문제/Level 3/다단계 칫솔 판매.py
+++ Progammers/연습문제/Level 3/다단계 칫솔 판매.py
@@ -4,22 +4,22 @@ def solution(enroll, referral, seller, amount):
             self.name = name
             self.money = money
             self.parent = parent
-            self.children = {}
             
-    tree = {'-': Node('-')}
+    tree = {'-': Node(name='-')}
     for enr, ref in zip(enroll, referral):
         tree[enr] = Node(name=enr, parent=ref)
-        tree[ref].children[enr] = True
         
-    for seller, amount in zip(seller, amount):
-        revenue = amount * 100
+    for curr, amnt in zip(seller, amount):
+        revenue = amnt * 100
+        while tree[curr].parent is not None:
+            reward = int(revenue * 0.1)
+            tree[curr].money += revenue - reward
+            revenue = reward
+            curr = tree[curr].parent
+    
+    del tree['-']
+    return [tree[key].money for key in tree]
         
-        
-    
-    
-    
-    
-
 
 enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
 referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
