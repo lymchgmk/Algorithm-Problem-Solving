2d76531b06d923b45d4e3a3653613b345e54e203
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-05-06 03:02:05 +0900

    Update 3 x n 타일링.py

--- Progammers/연습문제/Level 4/3 x n 타일링.py
+++ Progammers/연습문제/Level 4/3 x n 타일링.py
@@ -1,5 +1,23 @@
 def solution(n):
-    if n%2 == 1:
+    if n % 2 == 1:
         return 0
     
-    dp = [0]*n
+    dp = [0] * (n+1)
+    dp[0] = 1
+    prev = 0
+    for i in range(2, n+1, 2):
+        dp[i] = (3*dp[i-2] + 2*prev) % 1000000007
+        prev += dp[i-2]
+    
+    return dp[n]
+
+
+'''
+def solution(n):
+    if n % 2:
+        return 0
+    front = back = 1
+    for _ in range(n//2):
+        front, back = back, (4*back - front) % 1000000007
+    return back
+'''
