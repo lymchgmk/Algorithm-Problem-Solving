84709ba7cccc8c968c5e5f584cc85cadd397fa18
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-06-25 11:01:46 +0900

    Update 110 옮기기.py

--- Progammers/연습문제/Level 3/110 옮기기.py
+++ Progammers/연습문제/Level 3/110 옮기기.py
@@ -20,31 +20,31 @@
 #         # tmp = re.search(r'(110)', x)
 #         # print(x, tmp)
 #         # print(re.split(r'(110)', x))
+import re
 
 
 def solution(s):
-    def slice_110(input_str):
-        for idx in range(len(input_str)-3, -1, -1):
-            if input_str[idx:idx+3] == '110':
-                return input_str[:idx] + input_str[idx+3:]
-    
     answer = []
     for x in s:
-        flag = True
-        while flag:
-            # 1. 제일 뒤의 '110' 찾기
-            curr_x, sliced_x = x, slice_110(x)
-            
-            # 2. '110' 삽입하면서 체크
-            for idx in range(len(sliced_x)):
-                post_x = sliced_x[:idx] + '110' + sliced_x[idx:]
-                if curr_x > post_x:
-                    x = post_x
-                    break
-            else:
-                flag = False
-                answer.append(curr_x)
-    
+        # 1. '110' 제거
+        cnt = 0
+        while True:
+            post_x = re.sub(pattern=r'(110)', repl='', string=x)
+            if x == post_x:
+                break
+            x = post_x
+            cnt += 1
+
+        print(x, cnt)
+        # 2. '110' 삽입
+        while cnt:
+            for i in range(len(x)-1, -1, -1):
+                if x[i] == '0':
+                    print(x)
+                    x = x[:i] + '110' + x[i:]
+                    print(x)
+            cnt -= 1
+
     return answer
         
         
