91c053434fac4a564048297fa59cef9f6a3d3c86
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-06-25 11:08:48 +0900

    Update 110 옮기기.py

--- Progammers/연습문제/Level 3/110 옮기기.py
+++ Progammers/연습문제/Level 3/110 옮기기.py
@@ -1,25 +1,3 @@
-# import re
-#
-#
-# def solution(s):
-#     # 1. 제일 뒤에 있는 110을 찾기
-#     for x in s:
-#         rev_x = x[::-1]
-#         rev_pattern = r'(011)'
-#         rev_finditer = re.finditer(rev_pattern, rev_x)
-#         while True:
-#             try:
-#                 rev_span = finditer.__next__().span()
-#                 x = x[:span[0]] + x[span[1]:]
-#                 print('x', x)
-#                 for idx in range(len(x)):
-#                     tmp = x[:idx] + '110' + x[idx:]
-#                     print(tmp)
-#             except StopIteration:
-#                 break
-#         # tmp = re.search(r'(110)', x)
-#         # print(x, tmp)
-#         # print(re.split(r'(110)', x))
 import re
 
 
@@ -40,9 +18,12 @@ def solution(s):
         while cnt:
             for i in range(len(x)-1, -1, -1):
                 if x[i] == '0':
-                    print(x)
-                    x = x[:i] + '110' + x[i:]
-                    print(x)
+                    x = x[:i+1] + '110' + x[i+1:]
+                    break
+            else:
+                # 예외처리?
+                x += '110'
+            print(x)
             cnt -= 1
 
     return answer
