db14e17bbb0ff7ea28c2d852340a79daf3018fe9
lymchgmk <58398099+lymchgmk@users.noreply.github.com>
2021-07-04 05:43:32 +0900

    Update 110 옮기기.py

--- Progammers/연습문제/Level 3/110 옮기기.py
+++ Progammers/연습문제/Level 3/110 옮기기.py
@@ -2,32 +2,32 @@ import re
 
 
 def solution(s):
-    answer = []
-    for x in s:
-        # 1. '110' 제거
+    def remove_110(x):
         cnt = 0
         while True:
-            post_x = re.sub(pattern=r'(110)', repl='', string=x)
-            if x == post_x:
+            _x = re.sub(pattern=r'(110)', repl='', string=x)
+            _cnt = (len(x) - len(_x)) // 3
+            if x == _x:
                 break
-            x = post_x
-            cnt += 1
+            x = _x
+            cnt += _cnt
+        return x, cnt
 
-        print(x, cnt)
-        # 2. '110' 삽입
-        while cnt:
-            for i in range(len(x)-1, -1, -1):
-                if x[i] == '0':
-                    x = x[:i+1] + '110' + x[i+1:]
-                    break
-            else:
-                # 예외처리?
-                x += '110'
-            print(x)
-            cnt -= 1
+    def insert_110(x, cnt):
+        for idx in range(len(x)-1, -1, -1):
+            if x[idx] == '0':
+                return x[:idx+1] + '110' * cnt + x[idx+1:]
+        else:
+            return '110' * cnt + x
 
+    answer = []
+    for _s in s:
+        _s, cnt = remove_110(_s)
+        _s = insert_110(_s, cnt)
+        answer.append(_s)
     return answer
-        
-        
-s = ["1110","100111100","0111111010"]
+
+
+s = ["1110", "100111100", "0111111010"]
+# s = ["1011110","01110","101101111010"]
 print(solution(s))
