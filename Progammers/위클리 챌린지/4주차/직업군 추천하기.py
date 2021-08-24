2a0ef5d15cc4c77615000286d41b609b670c9147
lymchgmk <lymchgmk@gmail.com>
2021-08-24 15:48:49 +0900

    Update 직업군 추천하기.py

--- Progammers/위클리 챌린지/4주차/직업군 추천하기.py
+++ Progammers/위클리 챌린지/4주차/직업군 추천하기.py
@@ -1,13 +1,22 @@
 def solution(table, languages, preference):
-    preference_dict = {key: value for key in languages for value in preference}
+    preference_dict = {key: value for key, value in zip(languages, preference)}
+    
     table_dict = {}
     for row in table:
-        key, *values = row.split()
-        table_dict[key] = values
+        key, *langs = row.split()
+        table_dict[key] = {lang: len(langs) - idx for idx, lang in enumerate(langs)}
         
-    print(table_dict)
-    print(preference_dict)
-
+    result_dict = {field: 0 for field in table_dict}
+    for field in table_dict:
+        for lang, weight in preference_dict.items():
+            try:
+                result_dict[field] += table_dict[field][lang] * weight
+            except KeyError:
+                continue
+    
+    answer = sorted(result_dict.items(), key=lambda x: (-x[1], x[0]))[0][0]
+    return answer
+        
 
 if __name__ == "__main__":
     table = ["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
