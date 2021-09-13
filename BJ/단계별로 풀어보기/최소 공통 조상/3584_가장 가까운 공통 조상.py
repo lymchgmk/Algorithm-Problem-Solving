d74dd6edf05d2ac1c89e09ded34d4e3bbfcfcfb7
lymchgmk <lymchgmk@gmail.com>
2021-09-13 23:24:10 +0900

    Update 3584_가장 가까운 공통 조상.py

--- BJ/단계별로 풀어보기/최소 공통 조상/3584_가장 가까운 공통 조상.py
+++ BJ/단계별로 풀어보기/최소 공통 조상/3584_가장 가까운 공통 조상.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 sys.stdin = open("3584_가장 가까운 공통 조상.txt")
 input = lambda: sys.stdin.readline().strip()
 
@@ -6,12 +7,21 @@ input = lambda: sys.stdin.readline().strip()
 T = int(input())
 for _ in range(T):
     N = int(input())
-    tree = {i: [] for i in range(1, N+1)}
+    parent = defaultdict(int)
     for _ in range(N-1):
         A, B = map(int, input().split())
-        tree[A].append(B)
+        parent[B] = A
 
     X, Y = map(int, input().split())
+    X_parents, Y_parents = [0, X], [0, Y]
+    while parent[X]:
+        X_parents.append(parent[X])
+        X = parent[X]
+    while parent[Y]:
+        Y_parents.append(parent[Y])
+        Y = parent[Y]
 
-    print(tree)
-    print(X, Y)
+    cnt = 1
+    while X_parents[-cnt] == Y_parents[-cnt]:
+        cnt += 1
+    print(X_parents[-cnt+1])
