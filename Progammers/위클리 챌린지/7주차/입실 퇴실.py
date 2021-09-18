d1d983e4c33f6a726177620b609b5ff4e485c65a
lymchgmk <lymchgmk@gmail.com>
2021-09-18 21:07:53 +0900

    Update 입실 퇴실.py

--- Progammers/위클리 챌린지/7주차/입실 퇴실.py
+++ Progammers/위클리 챌린지/7주차/입실 퇴실.py
@@ -1,17 +1,16 @@
 def solution(enter, leave):
     answer = [0] * len(enter)
-
-    room = []
+    room = set()
     e_idx = 0
     for l in leave:
         while l not in room:
-            room.append(enter[e_idx])
+            room.add(enter[e_idx])
             e_idx += 1
-        room.remove(l)
-        for p in room:
-            answer[p - 1] += 1
-        answer[l - 1] += len(room)
 
+        room.discard(l)
+        for man in room:
+            answer[man-1] += 1
+        answer[l-1] += len(room)
     return answer
 
 
