01c37326e82f994786dcbbe102a212ef2884fd10
lymchgmk <lymchgmk@gmail.com>
2021-10-25 22:56:19 +0900

    Update 피로도.py

--- Progammers/위클리 챌린지/12주차/피로도.py
+++ Progammers/위클리 챌린지/12주차/피로도.py
@@ -1,22 +1,21 @@
+from collections import deque, defaultdict
+
+
 def solution(k, dungeons):
-    cnt = 0
-    while dungeons:
-        # 현재 피로도로 갈 수 있는 곳
-        dungeons = [d for d in dungeons if d[0] <= k]
-
-        # 다음 갈 던전 고르기기
-        next_idx, can_enter = -1, -1
-        for idx, (req, cost) in enumerate(dungeons):
-            tmp_k = k - cost
-            tmp_can_enter = len([d for d in dungeons if d[0] <= tmp_k])
-            if can_enter < tmp_can_enter:
-                next_idx = idx
-        print(next_idx, dungeons)
-        if next_idx == -1:
-            return cnt
-
-        k -= dungeons[next_idx][1]
-        cnt += 1
+    L = len(dungeons)
+    visited = defaultdict(lambda: False)
+    DP = deque([[k, visited]])
+    result = 0
+    while DP:
+        tmp_k, tmp_visited = DP.popleft()
+        for i in range(L):
+            req, cost = dungeons[i]
+            if not tmp_visited[i] and tmp_k >= req and tmp_k > cost:
+                tmp_visited[i] = True
+                DP.append([tmp_k - cost, tmp_visited])
+            else:
+                result = max(result, len(tmp_visited))
+    return result
 
 
 if __name__ == "__main__":
