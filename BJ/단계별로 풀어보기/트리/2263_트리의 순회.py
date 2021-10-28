1e89b2a6a549be74f869cf2068e0455fcd1d353d
lymchgmk <lymchgmk@gmail.com>
2021-10-28 23:18:38 +0900

    2263_트리의 순회

---
+++ BJ/단계별로 풀어보기/트리/2263_트리의 순회.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open("2263_트리의 순회.txt", "rt")
+input = lambda: sys.stdin.readline()
+sys.setrecursionlimit(10**9)
+
+
+n = int(input())
+inorder = list(map(int, input().split()))
+postorder = list(map(int, input().split()))
+
+pos = [0]*(n+1)
+for i in range(n):
+    pos[inorder[i]] = i
+
+
+def preorder(in_start, in_end, post_start, post_end):
+    if in_start > in_end or post_start > post_end:
+        return
+
+    parent = postorder[post_end]
+    print(parent, end=' ')
+    left = pos[parent] - in_start
+    right = in_end - pos[parent]
+
+    preorder(in_start, in_start + left - 1, post_start, post_start + left - 1)
+    preorder(in_end - right + 1, in_end, post_end - right, post_end - 1)
+
+
+preorder(0, n-1, 0, n-1)
