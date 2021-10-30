1e8a39d9fadcaedc53ff17805e9d862c718f0b2b
lymchgmk <lymchgmk@gmail.com>
2021-10-30 23:53:55 +0900

    Update 교점에 별 만들기.py

--- Progammers/위클리 챌린지/10주차/교점에 별 만들기.py
+++ Progammers/위클리 챌린지/10주차/교점에 별 만들기.py
@@ -2,35 +2,33 @@ from itertools import combinations
 
 
 def make_point(l1, l2):
-    A, B, E = map(float, l1)
-    C, D, F = map(float, l2)
+    A, B, E = l1
+    C, D, F = l2
     if A*D - B*C != 0:
-        x = (B*F - E*D) / (A*D - B*C)
-        y = (E*C - A*F) / (A*D - B*C)
-        if x == int(x) and y == int(y):
-            return int(x), int(y)
-
-def draw_stars(points):
-    Y, X = map(list, zip(*points))
-    Y.sort()
-    X.sort()
-    Y_min, Y_max = Y[0], Y[-1]
-    X_min, X_max = X[0], X[-1]
-    res = [['.']*(Y_max - Y_min + 1) for _ in range(X_max - X_min + 1)]
-    zp_y, zp_x = (Y_max - Y_min) // 2, (X_max - X_min) // 2
-    for py, px in points:
-        res[zp_x - px][zp_y + py] = '*'
-
-    return [''.join(r) for r in res]
+        p, r = divmod((B*F - E*D), (A*D - B*C))
+        q, s = divmod((E*C - A*F), (A*D - B*C))
+        if r == 0 and s == 0:
+            return p, -q
 
 
 def solution(line):
     points = []
+    INF = float('inf')
+    r_min, r_max, c_min, c_max = INF, -INF, INF, -INF
     for l1, l2 in combinations(line, 2):
         point = make_point(l1, l2)
         if point:
+            c, r = point
+            r_min = min(r_min, r)
+            r_max = max(r_max, r)
+            c_min = min(c_min, c)
+            c_max = max(c_max, c)
             points.append(point)
-    return draw_stars(points)
+
+    answer = [['.'] * (c_max - c_min + 1) for _ in range(r_max - r_min + 1)]
+    for c, r in points:
+        answer[r - r_min][c - c_min] = '*'
+    return [''.join(a) for a in answer]
 
 
 if __name__ == "__main__":
