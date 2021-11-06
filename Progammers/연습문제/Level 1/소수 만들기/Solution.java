d532dff7d5549c8932bb91adc6cc380b2bc5aba0
lymchgmk <lymchgmk@gmail.com>
2021-11-06 11:20:04 +0900

    [Programmers][Level 1][Java] 소수 만들기

--- Progammers/연습문제/Level 1/소수 만들기/Solution.java
+++ Progammers/연습문제/Level 1/소수 만들기/Solution.java
@@ -1,28 +1,28 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] combinations(int[] nums, int target){
-        boolean[] visited = new boolean[nums.length];
-        for(int r=1; r<=nums.length; r++){
-            
-        }
-    }
-    public boolean isPrime(int target) {
-        for(int div=0; div<=Math.sqrt(target)+1; div++) {
-            if(target % div == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
     public int solution(int[] nums) {
-        int answer = 0;
-        for(int[] comb : Combinations(nums, 3)){
-            int sum = Arrays.stream(comb).sum();
-            if(isPrime(sum)){
-                answer += 1;
+        int ans = 0;
+
+        for(int i = 0; i < nums.length - 2; i ++){
+            for(int j = i + 1; j < nums.length - 1; j ++){
+                for(int k = j + 1; k < nums.length; k ++ ){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
+                        ans += 1;  
+                    } 
+                }
             }
         }
-        return answer;
+        return ans;
+    }
+    public boolean isPrime(int target){
+        boolean flag = true;
+        for(int d=2; d<=(int)Math.sqrt(target); d++){
+            if(target % d == 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 }
