54122663fe6d0b8233a287a22ed3a67e2eec4822
lymchgmk <lymchgmk@gmail.com>
2022-02-07 22:50:18 +0900

    [BackJoon][Python] 9095_1,2,3 더하기

---
+++ BaekJoon/분류 안 된 문제들/다이나믹 프로그래밍/9095_1,2,3 더하기.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("9095_1,2,3 더하기.txt", "rt")
+
+
+def solution(n):
+    DP = [0, 1, 2, 4]
+    if n <= 3:
+        return DP[n]
+
+    DP += [0] * (n-3)
+    for i in range(4, n+1):
+        DP[i] = DP[i-1] + DP[i-2] + DP[i-3]
+    return DP[n]
+
+
+if __name__ == "__main__":
+    input = lambda: sys.stdin.readline().strip()
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        print(n, solution(n))
