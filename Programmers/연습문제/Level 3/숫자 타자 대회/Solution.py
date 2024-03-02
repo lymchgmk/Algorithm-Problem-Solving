71a39349f87aa7e416ccbc61ed1059662ded5e90
changmuk.im <changmuk.im>
2024-03-03 02:13:10 +0900

    SAVE

--- Programmers/연습문제/Level 3/숫자 타자 대회/Solution.py
+++ Programmers/연습문제/Level 3/숫자 타자 대회/Solution.py
@@ -1,68 +1,69 @@
-NUMBER_PAD = [
-    ('1', '2', '3'),
-    ('4', '5', '6'),
-    ('7', '8', '9'),
-    ('*', '0', '#')
-]
-
-CACHED_POS = {}
-NO_MOVE, UDLR_MOVE, DIA_MOVE = 1, 2, 3
+NUMBER_PAD = (
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+    (None, 0, None)
+)
+MAX_NUMBER_PAD_R, MAX_NUMBER_PAD_C = len(NUMBER_PAD), len(NUMBER_PAD[0])
+ALL_NUMBERS = range(10)
+START_LEFT_THUMB, START_RIGHT_THUMB = 4, 6
+CACHED_POSITION, CACHED_DISTANCE = {}, {}
+NO_MOVE_WEIGHT, UDLR_MOVE_WEIGHT, DIA_MOVE_WEIGHT = 1, 2, 3
 INF = float('inf')
 
 
-def solution(numbers):
-    thumb_l, thumb_r = [1, 0], [1, 2]
-    total_time = 0
-    number_count, max_number_count = 0, len(numbers)
-    stack = [(thumb_l, thumb_r, total_time, number_count)]
-    total_time = INF
-    while stack:
-        curr_thumb_l, curr_thumb_r, curr_total_time, curr_number_count = stack.pop()
+def solution(numbers: str) -> int:
+    dp = [[[INF for _ in ALL_NUMBERS] for _ in ALL_NUMBERS] for _ in range(len(numbers) + 1)]
+    dp[0][START_LEFT_THUMB][START_RIGHT_THUMB] = 0
 
-        if curr_number_count == max_number_count:
-            total_time = min(total_time, curr_total_time)
-            continue
+    for length, number in enumerate(map(int, numbers), start=1):
+        for left_thumb in ALL_NUMBERS:
+            for right_thumb in ALL_NUMBERS:
+                if left_thumb == right_thumb:
+                    continue
 
-        target_number = numbers[curr_number_count]
-        target_position = calc_number_position(target_number, NUMBER_PAD)
+                if dp[length - 1][left_thumb][right_thumb] == INF:
+                    continue
 
-        if target_position == curr_thumb_l or target_position == curr_thumb_r:
-            stack.append((curr_thumb_l, curr_thumb_r, curr_total_time + NO_MOVE, curr_number_count + 1))
-            continue
+                dp[length][number][right_thumb] = min(
+                    dp[length][number][right_thumb],
+                    dp[length - 1][left_thumb][right_thumb] + calc_distance(left_thumb, number)
+                )
+                dp[length][left_thumb][number] = min(
+                    dp[length][left_thumb][number],
+                    dp[length - 1][left_thumb][right_thumb] + calc_distance(right_thumb, number)
+                )
 
-        time_l, time_r = calc_time(curr_thumb_l, target_position), calc_time(curr_thumb_r, target_position)
-        if time_l < time_r:
-            stack.append((target_position, curr_thumb_r, curr_total_time + time_l, curr_number_count + 1))
-        elif time_l > time_r:
-            stack.append((curr_thumb_l, target_position, curr_total_time + time_r, curr_number_count + 1))
-        else:
-            stack.append((target_position, curr_thumb_r, curr_total_time + time_l, curr_number_count + 1))
-            stack.append((curr_thumb_l, target_position, curr_total_time + time_r, curr_number_count + 1))
-
-    return total_time
+    return min(map(min, dp[-1]))
 
 
-def calc_number_position(number: str, pad: [[str]]) -> (int, int):
-    if number in CACHED_POS:
-       return CACHED_POS[number]
+def calc_number_position(number: int) -> (int, int):
+    if number in CACHED_POSITION:
+        return CACHED_POSITION[number]
 
-    for r in range(len(pad)):
-        for c in range(len(pad[0])):
-            if pad[r][c] == number:
-                CACHED_POS[number] = (r, c)
-                return r, c
+    for r in range(MAX_NUMBER_PAD_R):
+        for c in range(MAX_NUMBER_PAD_C):
+            if NUMBER_PAD[r][c] == number:
+                CACHED_POSITION[number] = (r, c)
+                return CACHED_POSITION[number]
 
 
-def calc_time(thumb_pos, target_pos):
+def calc_distance(thumb: int, target: int) -> int:
+    thumb_pos, target_pos = calc_number_position(thumb), calc_number_position(target)
+    if (thumb_pos, target_pos) in CACHED_DISTANCE:
+        return CACHED_DISTANCE[(thumb_pos, target_pos)]
+
     diff_r, diff_c = abs(thumb_pos[0] - target_pos[0]), abs(thumb_pos[1] - target_pos[1])
-
     if diff_r == 0 and diff_c == 0:
-        return NO_MOVE
+        _distance = NO_MOVE_WEIGHT
     elif diff_r == 0 or diff_c == 0:
-        return max(diff_r, diff_c) * UDLR_MOVE
+        _distance = max(diff_r, diff_c) * UDLR_MOVE_WEIGHT
     else:
         _dia = min(diff_r, diff_c)
-        return _dia * DIA_MOVE + (max(diff_r, diff_c) - _dia) * UDLR_MOVE
+        _distance = _dia * DIA_MOVE_WEIGHT + (max(diff_r, diff_c) - _dia) * UDLR_MOVE_WEIGHT
+
+    CACHED_DISTANCE[(thumb_pos, target_pos)] = _distance
+    return _distance
 
 
 if __name__ == "__main__":
